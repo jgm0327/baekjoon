4a7ac33c49ce4db3cd21ebe385a02bf6c25575d4
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2022-10-15 15:12:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,10 @@
+def solution(num):
+    answer = 0
+    for i in range(500):
+        if num == 1:
+            return i
+        if num % 2:
+            num = num * 3 + 1
+        else:
+            num //= 2
+    return -1
