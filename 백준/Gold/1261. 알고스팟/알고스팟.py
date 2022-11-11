3a7fa654c96607ee1adba6a1496c420de39200ce
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2022-11-11 17:10:06 +0900

    [Gold IV] Title: 알고스팟, Time: 108 ms, Memory: 32908 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,5 +1,5 @@
+import heapq
 import sys
-from collections import deque
 
 input = sys.stdin
 n, m = map(int, input.readline().split())
@@ -16,23 +16,20 @@ def bfs():
     costs = [[int(1e3)] * n for _ in range(m)]
     costs[0][0] = 0
     dy, dx = [0, 0, 1, -1], [1, -1, 0, 0]
-    que = deque()
-    que.append([0, 0, 0])
+    heap = []
+    heapq.heappush(heap, [0, 0, 0])
 
-    while que:
-        y, x, cnt = que.popleft()
+    while heap:
+        cnt, y, x = heapq.heappop(heap)
         if cnt > costs[y][x] or (y == m - 1 and x == n - 1):
             continue
         for i in range(4):
             next_y, next_x = y + dy[i], x + dx[i]
-            if (0 > next_y or next_y >= m) or (0 > next_x or next_x >= n):
+            if (0 > next_y or next_y >= m) or (0 > next_x or next_x >= n) or \
+                    (graph[y][x] + costs[y][x]) >= costs[next_y][next_x]:
                 continue
-            if graph[next_y][next_x] and costs[next_y][next_x] > 1 + costs[y][x]:
-                costs[next_y][next_x] = costs[y][x] + 1
-                que.append([next_y, next_x, costs[next_y][next_x]])
-            elif not graph[next_y][next_x] and costs[next_y][next_x] >= 1 + costs[y][x]:
-                costs[next_y][next_x] = costs[y][x]
-                que.append([next_y, next_x, costs[next_y][next_x]])
+            costs[next_y][next_x] = costs[y][x] + graph[next_y][next_x]
+            heapq.heappush(heap, [costs[next_y][next_x], next_y, next_x])
 
     return costs[m - 1][n - 1]
 
