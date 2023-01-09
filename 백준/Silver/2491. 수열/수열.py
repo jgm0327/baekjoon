2a6cc2f0cbb8c2cf18a4dcfb554682b253ebaa7a
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-01-09 15:16:09 +0900

    [Silver IV] Title: 수열, Time: 144 ms, Memory: 32304 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin
+
+n = int(input.readline())
+num_list = list(map(int, input.readline().split()))
+dp = [0] * n
+dp[0] = num_list[0]
+asc = desc = 1
+max_value = 1
+
+for i in range(1, n):
+    if dp[i - 1] == num_list[i]:
+        asc += 1
+        desc += 1
+    elif dp[i - 1] > num_list[i]:
+        asc += 1
+        desc = 1
+        max_value = max(max_value, desc, asc)
+    else:
+        asc = 1
+        desc += 1
+        max_value = max(max_value, asc, desc)
+    dp[i] = num_list[i]
+    max_value = max(asc, desc, max_value)
+print(max_value)
