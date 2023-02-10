5ac5b730b58db9c06d4050e06aacd789ccd473a1
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-02-10 16:36:35 +0900

    [Silver II] Title: N과 M (9), Time: 76 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -3,27 +3,24 @@ from sys import stdin
 n, m = map(int, stdin.readline().split())
 numbers = sorted(stdin.readline().rstrip().split(), key=lambda x: int(x))
 count = {}.fromkeys(numbers, 0)
-visit_result = {}
-visit_idx = [False] * n
+visit = [False] * n
 
 def backtracking(depth: int, path: list) -> None:
     global n, m, numbers, count
     if depth == m:
-        result = ' '.join(path)
-        if visit_result.get(result) is None:
-            print(result)
-        visit_result[result] = True
+        print(' '.join(path))
         return
-
+    temp = 0
     for idx, number in enumerate(numbers):
-        if count[number] >= m or visit_idx[idx]:
+        if count[number] >= m or visit[idx] or temp == number:
             continue
         count[number] += 1
-        visit_idx[idx] = True
+        visit[idx] = True
         path.append(number)
+        temp = number
         backtracking(depth + 1, path)
         path.pop()
-        visit_idx[idx] = False
+        visit[idx] = False
         count[number] -= 1
 
 backtracking(0, [])
