cc22960249f7017dadb2a6105c2c42597288d044
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-02-16 18:52:56 +0900

    [Silver I] Title: IOIOI, Time: 176 ms, Memory: 116240 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,13 +3,14 @@ from collections import deque
 
 n = int(stdin.readline())
 m = int(stdin.readline())
-target = deque(list('IOI' + 'OI' * (n - 1)))
+
+target = 'IOI' + 'OI' * (n - 1)
 s = stdin.readline().rstrip()
 end = len(target)
 answer = 0
 comp = deque(list(s[:end]))
 while True:
-    if comp == target:
+    if ''.join(comp) == target:
         answer += 1
     if end >= m:
         break
