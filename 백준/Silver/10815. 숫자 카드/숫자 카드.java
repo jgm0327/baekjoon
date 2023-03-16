b1079341be48758c6da48a20669617f4cfe0115c
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-03-16 21:29:38 +0900

    [Silver V] Title: 숫자 카드, Time: 1124 ms, Memory: 184032 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		try {
 			br.readLine();
 			Integer[] arr = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
@@ -18,8 +21,10 @@ public class Main {
 			Integer[] target = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
 			for(int data : target) {
 				int answer = map.containsKey(data) ? 1 : 0;
-				System.out.print(answer + " ");
+				bw.write(answer + " ");
 			}
+			br.close();
+			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
