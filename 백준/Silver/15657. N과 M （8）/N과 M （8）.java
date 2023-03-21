7f364ad88991e24c12b50f875ce2c4d8f4ea97c6
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-03-21 12:52:32 +0900

    [Silver III] Title: N과 M (8), Time: 248 ms, Memory: 21404 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+	static int n, m;
+	static List<Integer> list;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] arr = br.readLine().split(" ");
+			n = Integer.parseInt(arr[0]);
+			m = Integer.parseInt(arr[1]);
+			list = new ArrayList<>();
+			String[] str = br.readLine().split(" ");
+			for(String s : str) list.add(Integer.parseInt(s));
+			Collections.sort(list);
+			back(0, 0, new LinkedList<Integer>());
+			br.close();
+			bw.close();
+		}catch(IOException e) {
+			
+		}
+	}
+	
+	private static void back(int depth, int start, LinkedList<Integer> path) {
+		if(depth == m) {
+			try {
+				StringBuilder str = new StringBuilder();
+				for(int p : path) str.append(p + " ");
+				bw.write(str.toString() + "\n");
+			}catch(IOException e) {
+			}
+			return;
+		}
+		
+		for(int i=start ; i<n ; i++) {
+			path.add(list.get(i));
+			back(depth+1, i, path);
+			path.pollLast();
+				
+		}
+	}
+}
