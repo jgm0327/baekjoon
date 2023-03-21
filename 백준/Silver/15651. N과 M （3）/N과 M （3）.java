4697c18518805e3c5557056a0b8ced238148d61f
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-03-21 12:21:32 +0900

    [Silver III] Title: N과 M (3), Time: 580 ms, Memory: 102720 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int n, m;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String[] arr = br.readLine().split(" ");
+			n = Integer.parseInt(arr[0]);
+			m = Integer.parseInt(arr[1]);
+			back(0, new StringBuilder());
+			br.close();
+			bw.close();
+		}catch(IOException e) {
+			
+		}
+	}
+	
+	private static void back(int depth, StringBuilder str) {
+		if(depth == m) {
+			try {
+				bw.write(str.toString() + "\n");
+			}catch(IOException e) {
+			}
+			return;
+		}
+		
+		for(int i=1 ; i<=n ; i++) {
+			str.append(i + " ");
+			back(depth+1, str);
+			str.delete(2*depth, 2*depth + 2);
+		}
+	}
+}
