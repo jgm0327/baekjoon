4aa4bd9e0da9830a3974c8b4ada8d9aa9be8cc53
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-04-17 12:41:50 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18152 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	private static int answer = Integer.MAX_VALUE;
-	private static int[] dp;
-	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
-		 recur(n, 0);
-		System.out.println(answer);
-		br.close();
-	}
-	
-	private static void recur(int num, int depth) {
-		if(dp[num] != 0 && dp[num] <= depth)return;
-		if(num <= 1) {
-			if(num == 1)
-				answer = Math.min(answer, depth);
-			return;
+		int[] dp = new int[n+4];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[1] = 0;
+		dp[2] = dp[3] = 1;
+		for(int i = 4; i <= n ; i++) {
+			if(i % 3 == 0)dp[i] = dp[i / 3] + 1;
+			if(i % 2 == 0)dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			dp[i] = Math.min(dp[i], dp[i - 1] + 1);
 		}
-		dp[num] = depth;
-		if(num % 3 == 0)recur(num / 3, depth+1);
-		if(num % 2 == 0)recur(num / 2, depth+1);
-		recur(num-1, depth+1);
+		System.out.println(dp[n]);
+		br.close();
 	}
 }
