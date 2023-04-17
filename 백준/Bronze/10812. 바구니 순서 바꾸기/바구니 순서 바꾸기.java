1c56f49d68d2fd60b3baf4f7e35c35c8498a336f
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-04-17 15:34:02 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 148 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.java
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
@@ -16,7 +13,6 @@ public class Main {
 		int n, m;
 		n = Integer.parseInt(size[0]);
 		m = Integer.parseInt(size[1]);
-		
 		
 		for(int i=0 ; i<=n ; i++)
 			list.add(i);
@@ -41,5 +37,4 @@ public class Main {
 		}
 		br.close();
 	}
-	
 }
