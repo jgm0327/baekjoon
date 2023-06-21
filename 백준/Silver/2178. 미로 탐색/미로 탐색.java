1472c4cee9979ddfa4a01b3c1d5d3349d39c5471
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-06-21 16:47:25 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,47 +4,48 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Main {
+class Main{
+  private static int n, m;
+  private static int[][] maze;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] size = br.readLine().split(" ");
+    n = Integer.parseInt(size[0]);
+    m = Integer.parseInt(size[1]);
 
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String[] str = br.readLine().split(" ");
-			int n = Integer.parseInt(str[0]), m = Integer.parseInt(str[1]);
-			int[][] graph = new int[n][m];
-			for(int i=0 ; i<n ; i++) {
-				String[] tmp = br.readLine().split("");
-				for(int j=0 ; j<m ; j++) {
-					graph[i][j] = Integer.parseInt(tmp[j]);
-				}
-			}
-			int answer = bfs(n, m, graph);
-			System.out.print(answer);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    maze = new int[n][m];
+    for(int i=0 ; i<n ; i++){
+      String str = br.readLine();
+      for(int j=0 ; j<m ; j++){
+        maze[i][j] = (str.charAt(j) - '0');
+      }
+    }
 
-	}
+    System.out.println(bfs());
 
-	private static int bfs(int n, int m, int[][] graph) {
-		Queue<int[]> que = new LinkedList<>();
-		que.add(new int[] {0, 0, 1});
-		int[] dy = {0,0,-1,1}, dx = {1,-1,0,0};
-		boolean[][] visit = new boolean[n][m];
-		visit[0][0] = true;
-		while(!que.isEmpty()) {
-			int[] cur = que.poll();
-			int y = cur[0], x = cur[1], cnt = cur[2];
-			for(int i=0 ; i<4 ; i++) {
-				int ny = dy[i] + y, nx = dx[i] + x;
-				if((0 <= ny && ny < n) && (0 <= nx && nx < m) && !visit[ny][nx] && graph[ny][nx] == 1) {
-					if(ny == n - 1 && nx == m - 1)return cnt + 1;
-					visit[ny][nx] = true;
-					que.add(new int[] {ny, nx, cnt + 1});
-				}
-			}
-		}
-		return 0;
-	}
+    br.close();
+  }
+
+  private static int bfs(){
+    Queue<int[]> que = new LinkedList<>();
+    que.add(new int[]{0,0,1});
+    boolean[][] visit = new boolean[n][m];
+    visit[0][0] = true;
+    final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
+
+    while(!que.isEmpty()){
+      int[] cur = que.poll();
+      int y = cur[0], x = cur[1], cnt = cur[2];
+      for(int i=0 ; i<4 ; i++){
+        int ny = y + dy[i], nx = x + dx[i];
+        if(ny == n - 1 && nx == m - 1)return cnt + 1;
+        if(ny < 0 || ny >= n || nx < 0 || nx >= m || visit[ny][nx] || maze[ny][nx] == 0)
+          continue;
+        visit[ny][nx] = true;
+        que.add(new int[]{ny, nx, cnt + 1});
+      }
+    }
+    return -1;
+  }
+
 }
