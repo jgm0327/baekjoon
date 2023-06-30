b5cf0d19b0eb77454f6e5bb62a2105c1ec90b521
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-06-30 16:52:47 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/README.md
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/README.md
@@ -1,10 +1,10 @@
 # [unrated] 조건에 맞게 수열 변환하기 2 - 181881 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181881?language=java) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.01 ms
+메모리: 66.7 MB, 시간: 0.01 ms
 
 ### 구분
 
---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            answer++;
+            for(int i=0 ; i<arr.length; i++){
+                if(arr[i] >= 50 && arr[i] % 2 == 0){
+                    arr[i] /= 2;
+                    flag = true;
+                }
+                else if(arr[i] < 50 && arr[i] % 2 == 1){
+                    arr[i] = arr[i] * 2 + 1;
+                    flag = true;
+                }
+            }
+        }
+        return answer - 1;
+    }
+}
