1c180f73c502155cd5a8b9fa8cac649576e660f1
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-07-04 17:27:53 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 372 ms, Memory: 32968 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,47 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.StringTokenizer;
 
-public class Main {
-	private static int n, k, answer = 0;
-	private static int[] arr;
-	private static int[] count;
-	
-	public static void main(String[] args) throws IOException{
-		init();
-		solution();
-		System.out.println(answer);
-	}
-	
-	private static void init() throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		count = new int[100001];
-		String[] size = br.readLine().split(" ");
-		n = Integer.parseInt(size[0]);
-		k = Integer.parseInt(size[1]);
-		
-		arr = new int[n];
-		int idx = 0;
-		for(String v : br.readLine().split(" ")) {
-			arr[idx++] = Integer.parseInt(v);
-		}
-		
-		br.close();
-	}
-	
-	private static void solution() {
-		int start = 0, end = 0;
-		while(start <= end && end < n) {
-			if(count[arr[end]] + 1 <= k) {
-				count[arr[end]]++;
-				end++;
-			}else {
-				count[arr[start]]--;
-				start++;
-			}
-			answer = Math.max(answer, end - start);
-		}
-	}
+class Main{
+  private static int n, k;
+  private static int[] arr;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+    arr = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for(int i=0 ; i<n ; i++){
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    System.out.println(solution());
+    br.close();
+  }
+
+  private static int solution(){
+    int[] count = new int[100001];
+    int start = 0, end = 0, ret=0;
+    while(end < n){
+      count[arr[end]]++;
+      while(start <= end && count[arr[end]] > k){
+        count[arr[start]]--;
+        start++;
+      }
+      ret = Math.max(ret, end - start + 1);
+      end++;
+    }
+    return ret;
+  }
 }
