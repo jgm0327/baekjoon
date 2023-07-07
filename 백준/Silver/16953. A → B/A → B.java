e84943c4d4e79be56f49e3b485132182f3073c6e
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-07-07 18:19:05 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		long n = sc.nextLong(), m = sc.nextLong();
-		int answer = 1;
-		
-		while (m > n) {
-			answer++;
-			if(m % 10 == 1)m /= 10;
-			else if(m % 2 == 0)m /= 2;
-			else break;
-		}
-		System.out.println(m == n ? answer : -1);
-	}
+class Main{
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] str = br.readLine().split(" ");
+    int n = Integer.parseInt(str[0]), m = Integer.parseInt(str[1]), answer=0;
+    while(m > n){
+      answer++;
+      if(m % 10 == 1)m /= 10;
+      else if (m % 2 == 0)m /= 2;
+      else break;
+    }
+    System.out.println(m == n ? answer + 1 : -1);
+    br.close();
+  }
 }
