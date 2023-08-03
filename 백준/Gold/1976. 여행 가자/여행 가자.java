279a6f4c2fafce622845e168fa2c1de2067d5741
jgm0327 <100139942+jgm0327@users.noreply.github.com>
2023-08-03 14:46:23 +0900

    [Gold IV] Title: 여행 가자, Time: 176 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,62 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
-	static int n, m;
-	static int[] parents, ranks;
+class Main{
+  private static int n, m;
+  private static List<Integer>[] countries;
+  private static int[] parents, path;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(br.readLine());
+    m = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			n = Integer.parseInt(br.readLine());
-			m = Integer.parseInt(br.readLine());
-			parents = new int[n + 1];
-			ranks = new int[n + 1];
-			
-			for(int i=0 ; i<=n ; i++) {
-				parents[i] = i;
-				ranks[i] = 1;
-			}
-			
-			for(int i=0 ; i<n ; i++) {
-				String[] str = br.readLine().split(" ");
-				for(int j=0 ; j<n ; j++) {
-					if(Integer.parseInt(str[j]) == 1) {
-						union(i+1, j+1);
-					}
-				}
-			}
-			String[] str = br.readLine().split(" ");
-			boolean flag = true;
-			for(int i=0 ; i<m-1 ; i++) {
-				if(findParent(Integer.parseInt(str[i])) != findParent(Integer.parseInt(str[i+1]))) {
-					flag = false;
-					break;
-				}
-			}
-			System.out.println(flag ? "YES" : "NO");
-			br.close();
-		}catch(IOException e) {
-			
-		}
-	}
-	
-	private static int findParent(int x) {
-		if(parents[x] == x)return x;
-		return parents[x] = findParent(parents[x]);
-	}
-	
-	private static void union(int x, int y) {
-		int px = findParent(x), py = findParent(y);
-		if(px == py)return;
-		
-		if(ranks[px] < ranks[py]) {
-			int temp = px;
-			px = py;
-			py = temp;
-		}
-		parents[py] = px;
-		ranks[py]++;
-	}
+    countries = new ArrayList[n + 1];
+    parents = new int[n + 1];
+    for(int i=0 ; i<=n ; i++){
+      parents[i] = i;
+      countries[i] = new ArrayList<>();
+    }
+
+    for(int i=0 ; i<n ; i++){
+      String[] values = br.readLine().split(" ");
+      for(int j=0 ; j<n ; j++){
+        if(Integer.parseInt(values[j]) == 0)continue;
+        if(findParent(i + 1) != findParent(j + 1))
+          union(i + 1, j + 1);
+      }
+    }
+
+    path = new int[m];
+    String[] values = br.readLine().split(" ");
+    for(int i=0 ; i<m ; i++){
+      path[i] = Integer.parseInt(values[i]);
+    }
+    System.out.println(print());
+    br.close();
+  }
+
+  private static String print(){
+    for(int i = 0 ; i < m - 1; i++){
+      if(findParent(path[i]) != findParent(path[i + 1]))
+        return "NO";
+    }
+    return "YES";
+  } 
+
+  private static int findParent(int x){
+    if(x == parents[x])return x;
+    return parents[x] = findParent(parents[x]);
+  }
+
+  private static void union(int x, int y){
+    int px = findParent(x), py = findParent(y);
+    if(px == py)return;
+
+    parents[px] = py;
+  }
 }
