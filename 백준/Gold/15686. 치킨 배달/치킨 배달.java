86de2125c1bda69c6b1d1b8eb4b12b62ade408c6
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-10-03 21:01:10 +0900

    [Gold V] Title: 치킨 배달, Time: 436 ms, Memory: 25232 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,40 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	private static int n, m, answer;
 	private static List<int[]> chickenHouses, houses;
-	private static Deque<int[]> open;
+	private static boolean[][] open;
 	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().split(" ");
-		n = Integer.parseInt(str[0]);
-		m = Integer.parseInt(str[1]);
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
 		answer = Integer.MAX_VALUE;
-
+		open = new boolean[n][n];
+		
 		chickenHouses = new ArrayList<>();
 		houses = new ArrayList<>();
-		open = new ArrayDeque<>();
 		
 		for(int i=0 ; i<n ; i++) {
-			String[] input = br.readLine().split(" ");
 			for(int j=0 ; j<n ;j++) {
-				int data = Integer.parseInt(input[j]);
+				int data = sc.nextInt();
 				if(data == 2)chickenHouses.add(new int[] {i, j});
 				else if(data == 1)houses.add(new int[] {i, j});
 			}
 		}
 		closed(0, 0);
 		System.out.println(answer);
-		br.close();
 	}
 	
 	private static void calcDist() {
@@ -42,15 +33,13 @@ public class Main {
 		for(int[] house : houses) {
 			int y = house[0], x = house[1];
 			tmp = Integer.MAX_VALUE;
-			Iterator<int[]> it = open.iterator();
-			while(it.hasNext()) {
-				int[] cur = it.next();
-				int cy = cur[0], cx = cur[1];
+			for(int[] chickenHouse : chickenHouses) {
+				int cy = chickenHouse[0], cx = chickenHouse[1];
+				if(!open[cy][cx])continue;
 				tmp = Math.min(tmp, (Math.abs(y - cy) + Math.abs(x - cx)));
 			}
 			comp += tmp;
 		}
-		
 		answer = Math.min(comp, answer);
 	}
 	
@@ -59,10 +48,12 @@ public class Main {
 			return;
 		}
 		for(int i=start ; i<chickenHouses.size() ; i++) {
-			open.add(chickenHouses.get(i));
+			int[] cur = chickenHouses.get(i);
+			int y = cur[0], x = cur[1];
+			open[y][x] = true;
 			calcDist();
 			closed(depth + 1, i + 1);
-			open.pollLast();
+			open[y][x] = false;
 		}
 	}
 }
