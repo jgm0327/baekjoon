2bed311479f269b05ee55021a699eb6cd4584e45
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-11-05 10:04:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,24 +1,30 @@
 import sys
 
-input = sys.stdin
 
-n, m = map(int, input.readline().split())
-lan_wires = []
-for _ in range(n):
-    lan_wires.append(int(input.readline()))
+def solution():
+    k, n = map(int, sys.stdin.readline().split())
+    lan_cables = []
+    for _ in range(k):
+        lan_cables.append(int(sys.stdin.readline()))
 
-start, end = 0, (2**31)
-answer = 0
-while start <= end:
-    mid = (start + end) // 2
-    count = 0
-    for lan_wire in lan_wires:
-        if mid == 0:
-            break
-        count += lan_wire // mid
-    if m > count:
-        end = mid - 1
-    else:
-        answer = answer if answer > mid else mid
-        start = mid + 1
-print(answer)
+    start, end = 0, (2**31 - 1)
+    result = 0
+
+    while start <= end:
+        total_cut_cable = 0
+        mid = int((start + end) / 2)
+
+        for cable in lan_cables:
+            if mid:
+                total_cut_cable += int(cable / mid)
+
+        if n > total_cut_cable:
+            end = mid - 1
+        else:
+            start = mid + 1
+            result = result if result > mid else mid
+
+    print(result)
+
+
+solution()
