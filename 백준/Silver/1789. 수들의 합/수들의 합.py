31f675bc35adcc919b6bc411b61b0f5929d19da9
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-11-16 17:44:08 +0900

    [Silver V] Title: 수들의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,15 +1,17 @@
-from sys import stdin
+n= int(input())
 
-n = int(stdin.readline())
-start, end = 0, n + 1
+left, right = 0, int(1e9)
+
 answer = 0
-while start <= end:
-    mid = (start + end) // 2
-    total = (mid * (mid + 1)) / 2
+while left <= right:
+    mid = (left + right) // 2
+
+    total = (mid * (mid + 1)) // 2
+
     if total > n:
-        end = mid - 1
+        right = mid - 1
     else:
-        start = mid + 1
         answer = mid
+        left = mid + 1
 
 print(answer)
