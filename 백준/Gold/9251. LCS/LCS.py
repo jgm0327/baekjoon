207273d62d2c93bd83aa72546158d6462c613c32
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-11-19 13:08:16 +0900

    [Gold V] Title: LCS, Time: 560 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,14 @@
-import sys
+a = input()
+b = input()
 
-input = sys.stdin
+n, m = len(a), len(b)
+dp = [[0] * (m + 1) for _ in range((n + 1))]
 
-paragraph1 = input.readline().rstrip()
-paragraph2 = input.readline().rstrip()
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-n1, n2 = len(paragraph1), len(paragraph2)
-
-dp = [[0] * (n2 + 1) for _ in range(n1 + 1)]
-
-for i in range(1, n1 + 1):
-    for j in range(1, n2 + 1):
-        dp[i][j] = dp[i - 1][j - 1] + 1 if paragraph1[i - 1] == paragraph2[j - 1] else max(dp[i - 1][j], dp[i][j - 1])
 print(dp[-1][-1])
