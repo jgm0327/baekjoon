74910cde1f6ecdd8b450249b7c7e0d3a6bf293bd
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-11-19 15:16:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,40 @@
-from sys import stdin
 from collections import deque
 
-n = int(stdin.readline())
-building = [list(stdin.readline().rstrip()) for _ in range(n)]
+n = int(input())
+
+graph = [list(input()) for _ in range(n)]
 
 
-def bfs(y: int, x: int) -> int:
-    global building, visit, n
-    dy, dx = [0, 0, -1, 1], [1, -1, 0, 0]
-    que = deque([[y, x]])
+def bfs(graph, visit, _y, _x):
+    global n
+    que = deque()
+    que.append((_y, _x))
+    visit[_y][_x] = True
+    dy, dx = [0, 0, 1, -1], [1, -1, 0, 0]
 
-    cnt = 1
+    ret = 0
+
     while que:
         cy, cx = que.popleft()
+        ret += 1
         for i in range(4):
             ny, nx = cy + dy[i], cx + dx[i]
-            if 0 <= ny < n and 0 <= nx < n and not visit[ny][nx] and building[ny][nx] == '1':
-                visit[ny][nx] = True
-                que.append([ny, nx])
-                cnt += 1
-    return cnt
-    
+            if 0 > ny or ny >= n or 0 > nx or nx >= n or visit[ny][nx] or graph[ny][nx] == '0':
+                continue
+            visit[ny][nx] = True
+            que.append((ny, nx))
+
+    return str(ret)
 
 
-visit = [[False] * n for _ in range(n)]
 answer = []
-for i in range(n):
-    for j in range(n):
-        if not visit[i][j] and building[i][j] == '1':
-            visit[i][j] = True
-            answer.append(bfs(i, j))
+visit = [[False] * n for _ in range(n)]
+
+for y in range(n):
+    for x in range(n):
+        if visit[y][x] or graph[y][x] == '0':
+            continue
+        answer.append(bfs(graph, visit, y, x))
 
 print(len(answer))
-for data in sorted(answer):
-    print(data)
+print('\n'.join(sorted(answer, key=lambda x: int(x))))
