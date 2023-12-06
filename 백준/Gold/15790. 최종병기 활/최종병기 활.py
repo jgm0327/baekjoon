878c9b892ec6017fc3f736e96dc86e6942098717
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-12-06 16:45:04 +0900

    [Gold II] Title: 최종병기 활, Time: 2472 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15790. 최종병기 활/최종병기 활.py
+++ 백준/Gold/15790. 최종병기 활/최종병기 활.py
@@ -6,17 +6,28 @@ left, right = 1, n
 answer = -1
 
 while left <= right:
-    mid = (left + right) // 2
+    mid = (left + right) // 2 # 활의 최소 길이
 
     for i in range(m):
+        # 시작점 저
         prev = slots[i]
         total = 0
-        
+
+        #원형이기 때문에 한 바퀴 돌림
         for j in range(i + 1, m + i + 1):
-            length = slots[j % m] - prev if slots[j % m] - prev > 0 else slots[j % m] - prev + n
+            # 양수이면 그대로 0 이거나 음수이면 +n
+            length = slots[j % m] - prev
+
+            if length <= 0:
+                length += n
+
+            # 최소 길이인 mid 보다 크다면 continue
             if length < mid:
                 continue
+
+            # 구간 나누기
             total += 1
+            # 시작점 변
             prev = slots[j % m]
             
         if total >= k:
