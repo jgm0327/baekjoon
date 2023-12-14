4fbf38e028be99d8292267f4f9124cd15b84b3de
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-12-14 12:26:53 +0900

    [Gold V] Title: 집합의 표현, Time: 476 ms, Memory: 51940 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,56 +1,62 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main {
-	static int n, m;
-	static int[] parents, ranks;
-	
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		try {
-			String[] str = br.readLine().split(" ");
-			n = Integer.parseInt(str[0]); m = Integer.parseInt(str[1]);
-			parents = new int[n + 1];
-			ranks = new int[n + 1];
-			
-			for(int i=0 ; i<=n ; i++) {
-				parents[i] = i;
-				ranks[i] = 1;
-			}
-			while(m-- > 0) {
-				str = br.readLine().split(" ");
-				int opt = Integer.parseInt(str[0]);
-				int parent = Integer.parseInt(str[1]);
-				int child = Integer.parseInt(str[2]);
-				if(opt == 0)union(parent, child);
-				else bw.write(findParent(parent) == findParent(child) ? "YES\n" : "NO\n");
-			}
-			br.close();
-			bw.close();
-		}catch(IOException e) {
-			
-		}
-	}
-	
-	private static int findParent(int x) {
-		if(parents[x] == x)return x;
-		return parents[x] = findParent(parents[x]);
-	}
-	
-	private static void union(int x, int y) {
-		int px = findParent(x), py = findParent(y);
-		if(px == py)return;
-		
-		if(ranks[px] < ranks[py]) {
-			int temp = px;
-			px = py;
-			py = temp;
-		}
-		parents[py] = px;
-		ranks[py]++;
-	}
+class Main{
+
+    private static int n, m;
+    private static int[] parents;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        parents = new int[n + 1];
+
+        for(int i=0 ; i<=n ; i++){
+            parents[i] = i;
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for(int i=0 ; i<m ; i++){
+            stk = new StringTokenizer(br.readLine());
+            int opt, a, b;
+
+            opt = Integer.parseInt(stk.nextToken());
+            a = Integer.parseInt(stk.nextToken());
+            b = Integer.parseInt(stk.nextToken());
+
+            if(opt == 1){
+                int pa = findParent(a), pb = findParent(b);
+                answer.append(pa == pb ? "YES\n" : "NO\n");
+                continue;
+            }
+            union(a, b);
+        }
+        
+        System.out.println(answer);
+    }
+
+    private static int findParent(int x){
+        if(x == parents[x]){
+            return x;
+        }
+
+        return parents[x] = findParent(parents[x]);
+    }
+
+    private static void union(int x, int y){
+        int px = findParent(x), py = findParent(y);
+
+        if(px == py){
+            return;
+        }
+
+        parents[py] = px;
+    }
 }
