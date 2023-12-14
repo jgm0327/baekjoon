4b193be7d5da51bd4c7c47486459a52001709872
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-12-14 12:58:13 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1596 ms, Memory: 333336 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -2,70 +2,74 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main {
-	private static int n;
-	private static int[] parents, rank;
-	private static Queue<int[]> pq;
-	
-	public static void main(String[] args) throws IOException{
-		init();
-		solution();
-	}
-	
-	private static void init() throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] size = br.readLine().split(" ");
-		n = Integer.parseInt(size[0]);
-		
-		parents = new int[n+1];
-		rank = new int[n+1];
-		for(int i=0 ; i<=n ; i++) {
-			parents[i] = i;
-			rank[i] = 0;
-		}
-		
-		int m = Integer.parseInt(size[1]);
-		
-		pq = new PriorityQueue<>((o1,o2) -> o1[2] - o2[2]);
-		
-		while(m-- > 0) {
-			String[] str = br.readLine().split(" ");
-			int sour = Integer.parseInt(str[0]), des = Integer.parseInt(str[1]), cost = Integer.parseInt(str[2]);
-			pq.add(new int[] {sour, des, cost});
-		}
-		br.close();
-	}
-	
-	private static int findParent(int x) {
-		if(x == parents[x])return x;
-		return parents[x] = findParent(parents[x]);
-	}
-	
-	private static void union(int x, int y) {
-		int nx = findParent(x), ny = findParent(y);
-		if(nx == ny)return;
-		if(rank[nx] < rank[ny]) {
-			int temp = nx;
-			nx = ny;
-			ny = temp;
-		}
-		parents[ny] = nx;
-		if(rank[ny] == rank[nx])rank[nx]++;
-	}
-	
-	private static void solution() {
-		int ret = 0, cost = 0;
-		while(!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int sour = cur[0], des = cur[1];
-			if(findParent(sour) == findParent(des))continue;
-			cost = cur[2];
-			union(sour, des);
-			ret += cost;
-		}
-		ret -= cost;
-		System.out.println(ret);
-	}
+class Main{
+
+    private static int n, m;
+    private static int[] parents;
+    private static PriorityQueue<int[]> pq;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        m = Integer.parseInt(stk.nextToken());
+
+        parents = new int[n + 1];
+        for(int i=0 ; i<=n ; i++){
+            parents[i] = i;
+        }
+
+        pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        while(m-- > 0){
+            stk = new StringTokenizer(br.readLine());
+
+            int u, v, cost;
+            u = Integer.parseInt(stk.nextToken());
+            v = Integer.parseInt(stk.nextToken());
+            cost = Integer.parseInt(stk.nextToken());
+            pq.add(new int[]{u, v, cost});
+        }
+
+        System.out.println(mst());
+    }
+
+    private static int mst(){
+        int ret = 0, last = 0;
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int u = cur[0], v = cur[1], cost = cur[2];
+
+            if(findParent(u) == findParent(v))
+                continue;
+
+            ret += cost;
+            last = cost;
+            union(u, v);
+        }
+
+        return ret - last;
+    }
+
+    private static int findParent(int x){
+        if(x == parents[x]){
+            return x;
+        }
+
+        return parents[x] = findParent(parents[x]);
+    }
+
+    private static void union(int x, int y){
+        int px = findParent(x), py = findParent(y);
+
+        if(px == py){
+            return;
+        }
+
+        parents[py] = px;
+    }
 }
