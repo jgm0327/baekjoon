743973bb7695611dc5453c6dca9795e111729223
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2023-12-26 10:54:49 +0900

    [Gold V] Title: LCS, Time: 532 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,16 @@
-a = input()
-b = input()
+string1 = input()
+string2 = input()
 
-n, m = len(a), len(b)
-dp = [[0] * (m + 1) for _ in range((n + 1))]
+n, m = len(string1), len(string2)
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if a[i - 1] == b[j - 1]:
-            dp[i][j] = dp[i-1][j-1] + 1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if string1[i - 1] == string2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+            continue
+        
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
 print(dp[-1][-1])
