6a373a54d0b1b52b3a16ab8d25ae3beb7a0b0ad5
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-01-19 09:11:53 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2268 ms, Memory: 61960 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -11,18 +11,20 @@ dp = [[0] * (n + 1) for _ in range(n + 1)]
 for i in range(n + 1):
     dp[i][i] = 1
 
-for i in range(1, n):
-    if numbers[i] == numbers[i + 1]:
-        dp[i][i + 1] = 1
+for i in range(n):
+    if numbers[i] != numbers[i + 1]:
+        continue
+    
+    dp[i][i + 1] = 1
 
 for i in range(n - 2, 0, -1):
-    for j in range(i + 2, n + 1):
-        if numbers[i] == numbers[j] and dp[i+1][j-1]:
-            dp[i][j] = 1
+    for j in range(1, n+1):
+        if not (numbers[i] == numbers[j] and dp[i + 1][j - 1]):
+            continue
 
+        dp[i][j] = 1
 
 for _ in range(int(input())):
-    s, e = map(int, input().split())
-    
+    s, e = map(int, input().split())    
+
     print(dp[s][e])
-        
