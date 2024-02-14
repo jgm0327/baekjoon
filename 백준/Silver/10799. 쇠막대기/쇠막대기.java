f906fb69b31e1b48ae3ff618185ce97dde7d6c77
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-02-14 13:32:33 +0900

    [Silver II] Title: 쇠막대기, Time: 168 ms, Memory: 16564 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -2,34 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Deque<Character> stk = new ArrayDeque<>();
-		String sticks = br.readLine();
-		int answer = 0 ;
-		char prev = '(';
-		
-		for(int i=0 ; i<sticks.length() ; i++) {
-			char ch = sticks.charAt(i);
-			if(ch == '(') {
-				stk.add(ch);
-			}else if(ch == ')') {
-				if(prev != ')') {
-					stk.pollLast();
-					answer += stk.size();
-				}else {
-					stk.pollLast();
-					answer++;
-				}
-			}
-			prev = ch;
-		}
-		System.out.println(answer);
-		br.close();
-	}
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String sticks = br.readLine();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        int answer = 0;
+        char prev = '(';
+
+        for(int i=0 ; i<sticks.length() ; i++){
+            char ch = sticks.charAt(i);
+            if(ch == '('){
+                stack.add(ch);
+            }
+            else{
+                if(prev == '('){
+                    stack.pollLast();
+                    answer += stack.size();
+                }else{
+                    stack.pollLast();
+                    answer++;
+                }
+            }
+
+            prev = ch;
+        }
+
+        System.out.println(answer);
+    }    
 }
