65b4a450d3f77b970a7f102baa24ec0295e72b30
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-02-28 17:53:17 +0900

    [Gold IV] Title: 알파벳, Time: 772 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,42 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
-class Main{
-  private static int n, m, answer;
-  private static char[][] board;
-  private static Map<Character, Boolean> visit;
-  private static final int[] dy = {0,0,-1,1}, dx = {1,-1,0,0};
+public class Main {
+    private static int n, m, answer;
+    private static char[][] board;
+    private static final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
 
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] size = br.readLine().split(" ");
-    n = Integer.parseInt(size[0]);
-    m = Integer.parseInt(size[1]);
-    board = new char[n][m];
-    for(int i=0 ; i<n ; i++){
-      String values = br.readLine();
-      for(int j=0 ; j<m ; j++){
-        board[i][j] = values.charAt(j);
-      }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] size = br.readLine().split(" ");
+        n = Integer.parseInt(size[0]);
+        m = Integer.parseInt(size[1]);
+
+        board = new char[n][m];
+        for(int i=0 ; i<n ; i++){
+            String str = br.readLine();
+            for(int j=0 ; j<m ; j++){
+                board[i][j] = str.charAt(j);
+            }
+        }
+
+        answer = 0;
+        dfs(1, 0, 0, 1 << (int)(board[0][0] - 'A'));
+
+        System.out.println(answer);
     }
-    visit = new HashMap<>();
-    visit.put(board[0][0], true);
-    dfs(1, 0, 0);
-    System.out.println(answer);
-    br.close();
-  }
 
-  private static void dfs(int depth, int y, int x){
-    answer = Math.max(answer, depth);
-    for(int i=0 ; i<4 ; i++){
-      int ny = y + dy[i], nx  = x + dx[i];
-      if(ny < 0 || ny >= n || nx < 0 || nx >= m || visit.containsKey(board[ny][nx]))continue;
-      visit.put(board[ny][nx], true);
-      dfs(depth + 1, ny, nx);
-      visit.remove(board[ny][nx]);
+    public static void dfs(int depth, int y, int x, int path){
+
+        answer = Math.max(depth, answer);
+        
+        for(int i=0 ; i<4 ; i++){
+            int ny = y + dy[i], nx = x + dx[i];
+            
+            if(0 > ny || ny >= n || 0 > nx || nx >= m)
+                continue;
+            
+            int bit = (int)(board[ny][nx] - 'A');
+
+            if((path & (1 << bit)) == (1 << bit))
+                continue;
+
+            path |= (1 << bit);
+            dfs(depth + 1, ny, nx, path);
+            path &= ~(1 << bit);
+        }
     }
-  }
 }
