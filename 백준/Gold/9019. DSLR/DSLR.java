02de27a466d452bb1cf6e2e2710a390039364ede
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-03-18 15:42:13 +0900

    [Gold IV] Title: DSLR, Time: 3196 ms, Memory: 302084 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -1,58 +1,68 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-	public class Main {
-		private static int T;
-		private static String[] answer;
-		
-		public static void main(String[] args) throws IOException{
-			init();
-		}
-		
-		private static void init() throws IOException{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			T = Integer.parseInt(br.readLine());
-			while(T-- > 0) {
-				String[] values = br.readLine().split(" ");
-				int start = Integer.parseInt(values[0]), target = Integer.parseInt(values[1]);
-				answer = new String[10003];
-				for(int i=0 ; i<=10002 ; i++)answer[i] = new String();
-				bw.write(bfs(start, target) + "\n");
-			}
-			bw.flush();
-			bw.close();
-			br.close();
-		}
-		
-		private static String bfs(int start, int target) {
-			Queue<Integer> que = new LinkedList<>();
-			que.add(start);
-			boolean[] visit = new boolean[10003];
-			visit[start] = true;
-			
-			while(!que.isEmpty()) {
-				int cur = que.poll();
-				int d = (cur * 2) % 10000, s = (cur == 0) ? 9999 : cur - 1, 
-						l = (cur % 1000) * 10 + cur / 1000,
-						r = (cur % 10) * 1000 + cur / 10;
-				
-				int[] arr = new int[] {d, s, l, r};
-				char[] opt = new char[] {'D', 'S', 'L', 'R'};
-				for(int i=0 ; i<4 ; i++) {
-					if(!visit[arr[i]]) {
-						answer[arr[i]] = answer[cur] + opt[i];
-						if(target == arr[i])return answer[arr[i]];
-						que.add(arr[i]);
-						visit[arr[i]] = true;
-					}
-				}
-			}
-			return "";
-		}
+class Main{
+    static class RegisterNumber{
+        int value;
+        StringBuilder path;
+
+        public RegisterNumber(int value, StringBuilder path){
+            this.value = value;
+            this.path = path;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder answer = new StringBuilder();
+        while(t-- > 0){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int origin = Integer.parseInt(stk.nextToken());
+            int target = Integer.parseInt(stk.nextToken());
+            answer.append(bfs(origin, target)).append("\n");
+        }
+
+        System.out.print(answer);
+    }
+
+    private static String bfs(int origin, int target){
+        Queue<RegisterNumber> que = new LinkedList<>();
+        boolean[] visit = new boolean[10000];
+        final char[] command = new char[]{'D', 'S', 'L', 'R'};
+
+        que.add(new RegisterNumber(origin, new StringBuilder()));
+
+        while(!que.isEmpty()){
+            RegisterNumber cur = que.poll();
+            
+            int D = (cur.value * 2) % 10000;
+            int S = cur.value == 0 ? 9999 : cur.value - 1;
+
+            int L = (cur.value % 1000) * 10 + (cur.value / 1000);
+            int R = (cur.value % 10) * 1000 + (cur.value / 10);
+
+            int[] numbers = new int[]{D, S, L, R};
+
+            for(int i=0 ; i<4 ; i++){
+                int nextValue = numbers[i];
+
+                if(nextValue == target)
+                    return cur.path.append(command[i]).toString();
+
+                if(visit[nextValue])continue;
+
+                visit[nextValue] = true;
+                que.add(new RegisterNumber(nextValue, new StringBuilder(cur.path).append(command[i])));
+            }
+        }
+
+        return "";
+    }
 }
