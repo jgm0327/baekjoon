98af1fd6319debe4b32f6ed71329323044310a6e
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-03-22 16:37:28 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,18 @@
-a = input().rstrip()
-b = input().rstrip()
+A = [''] + list(input())
+B = [''] + list(input())
 
-n, m = len(a), len(b)
+n = len(A)
+m = len(B)
 
-dp = [[0] * (m + 1) for _ in range(n + 1)]
+dp = [[0] * m for _ in range(n)]
 
-for i in range(1, n + 1):
-    for j in range(1, m + 1):
-        if a[i - 1] == b[j - 1]:
+for i in range(1, n):
+    for j in range(1, m):
+        if A[i] == B[j]:
             dp[i][j] = dp[i-1][j-1] + 1
             continue
+        
+        dp[i][j] = max(dp[i-1][j], dp[i][j - 1])
 
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-print(dp[n][m])
+print(dp[-1][-1])
