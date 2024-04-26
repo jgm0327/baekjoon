4bc591d890332445029c5ffd36bfd00f45db022f
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-04-26 12:08:46 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1560 ms, Memory: 332404 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -5,71 +5,69 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 class Main{
-
-    private static int n, m;
+    private static int n;
     private static int[] parents;
-    private static PriorityQueue<int[]> pq;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(stk.nextToken());
-        m = Integer.parseInt(stk.nextToken());
-
+        n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        
         parents = new int[n + 1];
-        for(int i=0 ; i<=n ; i++){
+        for(int i=1 ; i<=n ; i++){
             parents[i] = i;
         }
 
-        pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
         while(m-- > 0){
-            stk = new StringTokenizer(br.readLine());
+            tokenizer = new StringTokenizer(br.readLine());
+            int sour, des, weight;
 
-            int u, v, cost;
-            u = Integer.parseInt(stk.nextToken());
-            v = Integer.parseInt(stk.nextToken());
-            cost = Integer.parseInt(stk.nextToken());
-            pq.add(new int[]{u, v, cost});
+            sour = Integer.parseInt(tokenizer.nextToken());
+            des = Integer.parseInt(tokenizer.nextToken());
+            weight = Integer.parseInt(tokenizer.nextToken());
+
+            pq.add(new int[]{sour, des, weight});
         }
 
-        System.out.println(mst());
+        System.out.println(mst(pq));
     }
 
-    private static int mst(){
-        int ret = 0, last = 0;
+    private static int mst(PriorityQueue<int[]> pq){   
+        int ret = 0;
 
+        int temp = 0;
         while(!pq.isEmpty()){
             int[] cur = pq.poll();
-            int u = cur[0], v = cur[1], cost = cur[2];
+            int x = cur[0], y = cur[1], weight = cur[2];
 
-            if(findParent(u) == findParent(v))
+            if(findParent(x) == findParent(y))
                 continue;
-
-            ret += cost;
-            last = cost;
-            union(u, v);
+            
+            union(x, y);
+            temp = weight;
+            ret += weight;
         }
 
-        return ret - last;
+        return ret - temp;
     }
-
+    
     private static int findParent(int x){
-        if(x == parents[x]){
+        if(x == parents[x])
             return x;
-        }
-
         return parents[x] = findParent(parents[x]);
     }
 
     private static void union(int x, int y){
         int px = findParent(x), py = findParent(y);
 
-        if(px == py){
+        if(px == py)
             return;
-        }
 
         parents[py] = px;
     }
+
 }
