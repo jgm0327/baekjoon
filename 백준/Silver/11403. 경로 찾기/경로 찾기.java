95738c32872d8d78f24213ff20f73444a4c8a728
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-04-26 13:38:10 +0900

    [Silver I] Title: 경로 찾기, Time: 180 ms, Memory: 15320 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,45 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main{
-  private static int n;
-  private static int[][] graph;
-  private static long[][] costs;
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    n = Integer.parseInt(br.readLine());
-    graph = new int[n][n];
-    costs = new long[n][n];
-    for(int i=0 ;i<n ; i++)
-      Arrays.fill(costs[i], Integer.MAX_VALUE);
 
-    for(int i=0 ; i<n ; i++){
-      String[] values = br.readLine().split(" ");
-      for(int j=0 ; j<n ; j++){
-        graph[i][j] = Integer.parseInt(values[j]);
-        if(graph[i][j] == 1)costs[i][j] = 1;
-      }
-    }
-    
-    for(int k=0 ; k<n ; k++){
-      for(int i=0 ; i<n ; i++){
-        for(int j=0 ; j<n ; j++){
-          if(costs[i][j] <= costs[i][k] + costs[k][j])continue;
-          costs[i][j] = costs[i][k] + costs[k][j];
+    private static int n;
+    private static int[][] graph;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        graph = new int[n][n];
+
+        for(int i=0 ; i<n ; i++){
+            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+            for(int j=0 ; j<n ; j++){
+                graph[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
         }
-      }
+
+        for(int k=0 ; k<n ; k++){
+            for(int i=0 ; i<n ; i++){
+                for(int j=0 ; j<n ; j++){
+                    if(graph[i][k] == 0 || graph[k][j] == 0)
+                        continue;
+
+                    graph[i][j] = 1;
+                }
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<n ; j++){
+                answer.append(graph[i][j]).append(" ");
+            }
+            answer.append("\n");
+        }
+
+        System.out.print(answer);
     }
-    StringBuilder sb = new StringBuilder();
-    for(int i=0 ; i<n ; i++){
-      for(int j=0 ; j<n ; j++){
-        if(costs[i][j] >= Integer.MAX_VALUE)sb.append(0 + " ");
-        else sb.append(1 + " ");
-      }
-      sb.append("\n");
-    }
-    System.out.print(sb);
-    br.close();
-  }
 }
