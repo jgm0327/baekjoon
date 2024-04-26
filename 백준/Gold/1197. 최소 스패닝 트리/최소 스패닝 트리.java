0edf4b86ed5452b5ff21e71bd76ab36632d1d3fc
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-04-26 12:05:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 612 ms, Memory: 48508 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,57 +2,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main{
-  private static int n, m;
-  private static int[] parents;
-  private static Queue<int[]> pq;
+    private static int n;
+    private static int[] parents;
 
-  public static void main(String[] args) throws IOException{
-    init();
-    mst();
-  }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  private static void init() throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String[] size = br.readLine().split(" ");
-    n = Integer.parseInt(size[0]);
-    m = Integer.parseInt(size[1]);
-    pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-    for(int i=0 ; i<m ; i++){
-      String[] values = br.readLine().split(" ");
-      int sour = Integer.parseInt(values[0]), des = Integer.parseInt(values[1]), cost = Integer.parseInt(values[2]);
-      pq.add(new int[]{sour, des, cost});
+        n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        
+        parents = new int[n + 1];
+        for(int i=1 ; i<=n ; i++){
+            parents[i] = i;
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        while(m-- > 0){
+            tokenizer = new StringTokenizer(br.readLine());
+            int sour, des, weight;
+
+            sour = Integer.parseInt(tokenizer.nextToken());
+            des = Integer.parseInt(tokenizer.nextToken());
+            weight = Integer.parseInt(tokenizer.nextToken());
+
+            pq.add(new int[]{sour, des, weight});
+        }
+
+        System.out.println(mst(pq));
     }
 
-    parents = new int[n+1];
-    for(int i=1 ; i<=n ; i++)parents[i] = i;
-    br.close();
-  }
+    private static int mst(PriorityQueue<int[]> pq){   
+        int ret = 0;
 
-  private static void mst(){
-    int answer = 0;
-    while(!pq.isEmpty()){
-      int[] cur = pq.poll();
-      int sour = cur[0], des = cur[1], cost = cur[2];
-      if(findParents(sour) == findParents(des))continue;
-      union(sour, des);
-      answer += cost;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int x = cur[0], y = cur[1], weight = cur[2];
+
+            if(findParent(x) == findParent(y))
+                continue;
+            
+            union(x, y);
+            ret += weight;
+        }
+
+        return ret;
+    }
+    
+    private static int findParent(int x){
+        if(x == parents[x])
+            return x;
+        return parents[x] = findParent(parents[x]);
     }
 
-    System.out.println(answer);
-  }
+    private static void union(int x, int y){
+        int px = findParent(x), py = findParent(y);
 
-  private static int findParents(int x){
-    if(x == parents[x])return x;
-    return parents[x] = findParents(parents[x]);
-  }
+        if(px == py)
+            return;
 
-  private static void union(int x, int y){
-    int px = findParents(x), py = findParents(y);
-    if(px == py)return;
-    parents[py] = px;
-  }
+        parents[py] = px;
+    }
+
 }
