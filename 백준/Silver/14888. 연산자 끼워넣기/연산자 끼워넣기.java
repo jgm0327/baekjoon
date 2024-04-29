71d600ff93e79464777947bb256c75e632a4671b
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-04-29 11:22:25 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,63 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-	private static int n, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-	private static int[] arr, oper;
-	
-	public static void main(String[] args) throws IOException{
-		insert();
-		recur(1, arr[0]);
-		System.out.println(max + "\n" + min);
-	}
-	
-	private static void insert() throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		oper = new int[4];
-		arr = new int[n];
-		
-		int idx = 0;
-		for(String v : br.readLine().split(" ")) {
-			arr[idx++] = Integer.parseInt(v);
-		}
-		
-		idx = 0;
-		for(String v : br.readLine().split(" ")) {
-			oper[idx++] = Integer.parseInt(v);
-		}
-		br.close();
-	}
-	
-	private static void recur(int depth, int num) {
-		if(depth == n) {
-			max = Math.max(max, num);
-			min = Math.min(min, num);
-			return;
-		}
-		
-		if(oper[0] > 0) {
-			oper[0]--;
-			recur(depth+1, num + arr[depth]);
-			oper[0]++;
-		}
-		if(oper[1] > 0) {
-			oper[1]--;
-			recur(depth+1, num - arr[depth]);
-			oper[1]++;
-		}
-		if(oper[2] > 0) {
-			oper[2]--;
-			recur(depth+1, num * arr[depth]);
-			oper[2]++;
-		}
-		
-		if(oper[3] > 0) {
-			oper[3]--;
-			recur(depth+1, num / arr[depth]);
-			oper[3]++;
-		}
-	}
-	
+class Main{
+
+    private static int[] count, numbers;
+    private static int n, minValue, maxValue;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        numbers = new int[n];
+        count = new int[4];
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<n ;i++){
+            numbers[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        tokenizer = new StringTokenizer(br.readLine());
+        for(int i=0 ; i<4 ; i++){
+            count[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        minValue = Integer.MAX_VALUE;
+        maxValue = Integer.MIN_VALUE;
+        backtracking(1, numbers[0]);
+
+        System.out.println(maxValue);
+        System.out.println(minValue);
+    }
+
+    private static void backtracking(int depth, int total){
+        if(depth == n){
+            minValue = Math.min(minValue, total);
+            maxValue = Math.max(maxValue, total);
+            return;
+        }
+
+        if(count[0] > 0){
+            count[0] -= 1;
+            backtracking(depth + 1, total + numbers[depth]);
+            count[0] += 1;
+        }
+        if(count[1] > 0){
+            count[1] -= 1;
+            backtracking(depth + 1, total - numbers[depth]);
+            count[1] += 1;
+        }
+        if(count[2] > 0){
+            count[2] -= 1;
+            backtracking(depth + 1, total * numbers[depth]);
+            count[2] += 1;
+        }
+        if(count[3] > 0){
+            count[3] -= 1;
+            backtracking(depth + 1, total / numbers[depth]);
+            count[3] += 1;
+        }
+    }
 }
