9cad52bc451331ce35cc82ea73a90afa8f8ffe1f
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-05-03 15:50:21 +0900

    [Gold III] Title: 소수의 연속합, Time: 220 ms, Memory: 26868 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,40 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 class Main{
 
-    private static List<Integer> primes;
-    private static boolean[] isPrime;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        primes = new ArrayList<>();
-        isPrime = new boolean[n + 1];
-        findPrimes(n);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int start = 0, end = 0, total = 0, answer = !isPrime[n] ? 1 : 0;
-        while(!primes.isEmpty()){
-            if(start <= end && total >= n){
-                if(total == n)answer++;
-                total -= primes.get(start++);
-            }
-            else if(end == primes.size() - 1)break;
-            else total += primes.get(end++);
-        }
-        System.out.println(answer);
-        sc.close();
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(solution(findPrimes(n), n));
     }
 
-    private static void findPrimes(int n){
-        isPrime[1] = true;
+    private static List<Integer> findPrimes(int n){
+        List<Integer> ret = new ArrayList<>();
+
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+
+        primes[0] = primes[1] = false;
         for(int i=2 ; i<=n ; i++){
-            if(isPrime[i])continue;
-            primes.add(i);
-            for(int j=i+i ; j<=n ; j+=i){
-                isPrime[j] = true;
+            if(!primes[i])continue;
+
+            ret.add(i);
+            for(int j=i + i ; j<=n ; j += i){
+                primes[j] = false;
             }
         }
+
+        return ret;
+    }
+
+    private static int solution(List<Integer> primes, int n){
+        int ret = 0;
+
+        int left = 0, right = 0, total = 0;
+
+        while(left <= right){
+            if(total == n){
+                ret++;
+            }
+
+            if(total >= n)total -= primes.get(left++);
+            else {
+                if(right >= primes.size())break;
+                total += primes.get(right++);
+            }
+        }
+
+        return ret;
     }
 }
