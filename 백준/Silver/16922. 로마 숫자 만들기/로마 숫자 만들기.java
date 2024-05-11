1da420cecbb63e4df60b1ff16ca11baafbecc896
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-05-11 19:09:27 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Main{
-    private static Map<Integer, Map<Integer, Boolean>> visit;
-    private static Map<Integer, Boolean> answer;
+    private static boolean[][] visit;
+    private static int answer = 0;
     private static final int[] number = {1, 5, 10, 50};
 
     public static void main(String[] args) throws IOException{
@@ -14,28 +14,24 @@ class Main{
 
         int n = Integer.parseInt(br.readLine());
 
-        visit = new HashMap<>();
-        answer = new HashMap<>();
+        visit = new boolean[n][50 * 20 + 1];
 
         dfs(0, n, 0);
-        System.out.println(answer.size());
+        System.out.println(answer);
     }
 
     private static void dfs(int depth, int end, int total){
         if(depth == end){
-            answer.put(total, true);
+            answer++;
             return;
         }
 
         for(int i=0 ; i<4 ; i++){
             int nextTotal = total + number[i];
 
-            if(visit.containsKey(nextTotal) 
-            && visit.get(nextTotal).containsKey(depth))
-                continue;
-
-            if(!visit.containsKey(nextTotal))visit.put(nextTotal, new HashMap<>());
-            visit.get(nextTotal).put(depth, true);
+            if(visit[depth][nextTotal])continue;
+            
+            visit[depth][nextTotal] = true;
             dfs(depth + 1, end, nextTotal);
         }
     }
