06229b20db5e389f6553b3fe98804c14207ae069
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-05-11 19:10:21 +0900

    [Silver III] Title: 로마 숫자 만들기, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
+++ 백준/Silver/16922. 로마 숫자 만들기/로마 숫자 만들기.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 class Main{
     private static boolean[][] visit;
-    private static int answer = 0;
+    private static int answer = 0, n;
     private static final int[] number = {1, 5, 10, 50};
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
         visit = new boolean[n][50 * 20 + 1];
 
-        dfs(0, n, 0);
+        dfs(0, 0);
         System.out.println(answer);
     }
 
-    private static void dfs(int depth, int end, int total){
-        if(depth == end){
+    private static void dfs(int depth, int total){
+        if(depth == n){
             answer++;
             return;
         }
@@ -30,9 +28,9 @@ class Main{
             int nextTotal = total + number[i];
 
             if(visit[depth][nextTotal])continue;
-            
+
             visit[depth][nextTotal] = true;
-            dfs(depth + 1, end, nextTotal);
+            dfs(depth + 1, nextTotal);
         }
     }
 
