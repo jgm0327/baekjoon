cf1dd4ad7f84e22e47e1784e7ac06a87dc34735c
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-05-18 15:04:18 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -19,12 +19,14 @@ public class Main {
 
         String str = br.readLine();
         long answer = 0;
+
+        long pow = 1;
         for(int i=0 ; i<n ; i++){
             char ch = str.charAt(i);
-            answer = (answer + (long)Math.pow(31, i) * index.get(ch));
+            answer = (answer + (pow * index.get(ch)));
+            pow = (pow * 31) % 1234567891;
         }
-
-        System.out.println(answer);
+        System.out.println(answer % 1234567891);
 
     }
 }
