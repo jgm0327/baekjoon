fdd22852549b071286b2ccb86456d95409f6ecdd
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-05-20 12:29:41 +0900

    [Gold II] Title: 문제집, Time: 512 ms, Memory: 45832 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -8,53 +8,60 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static List<Integer>[] graph;
-	static int[] count;
-	static Queue<Integer> que;
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			StringTokenizer strtk = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(strtk.nextToken());
-			m = Integer.parseInt(strtk.nextToken());
-			count = new int[n+1];
-			graph = new ArrayList[n+1];
-			que = new PriorityQueue<>();
-			for(int i=0 ; i<=n ; i++)graph[i] = new ArrayList<Integer>();
-			for(int i=0 ; i<m ; i++) {
-				StringTokenizer tk = new StringTokenizer(br.readLine());
-				int sour = Integer.parseInt(tk.nextToken());
-				int des = Integer.parseInt(tk.nextToken());
-				graph[sour].add(des);
-				count[des]++;
-			}
-			for(int i=1; i <=n ; i++) {
-				if(count[i] == 0)que.add(i);
-			}
-			bfs();
-			br.close();
-		}catch(IOException e) {
-			
-		}
-	}
-	
-	private static void bfs() {
-		List<Integer> list = new ArrayList<>();
-		
-		while(!que.isEmpty()) {
-			int cur = que.poll();
-			list.add(cur);
-			for(int des : graph[cur]) {
-				count[des]--;
-				if(count[des] == 0)que.add(des);
-			}
-		}
-		
-		StringBuilder str = new StringBuilder();
-		for(int value : list) {
-			str.append(value + " ");
-		}
-		System.out.println(str.toString());
-	}
+    private static int[] pre;
+    private static int n, m;
+    private static Queue<Integer> pq;
+    private static List<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+
+        list = new ArrayList[n + 1];
+        for(int i=0 ; i<=n ; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        pre = new int[n + 1];
+        for(int i=0 ; i<m ; i++){
+            tokenizer = new StringTokenizer(br.readLine());
+            int sour = Integer.parseInt(tokenizer.nextToken());
+            int des = Integer.parseInt(tokenizer.nextToken());
+
+            pre[des]++;
+            list[sour].add(des);
+        }
+
+        pq = new PriorityQueue<>();
+        for(int i=1 ; i<=n ; i++){
+            if(pre[i] != 0)continue;
+
+            pq.add(i);
+        }
+
+        topologicalSort();
+    }
+
+    private static void topologicalSort(){
+        StringBuilder answer = new StringBuilder();
+
+        while(!pq.isEmpty()){
+            int sour = pq.poll();
+            answer.append(sour).append(" ");
+
+            for(int des : list[sour]){
+                pre[des]--;
+
+                if(pre[des] != 0)continue;
+
+                pq.add(des);
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
