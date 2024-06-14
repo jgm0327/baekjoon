701fca0db0f20911ce175f8c9245c3d4a1c5cff5
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-06-14 17:37:58 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 228 ms, Memory: 21188 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -1,114 +1,128 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 class Main{
+
     private static int n, m;
-    private static int[][] A;
-    private static final int[] dy = {0, 0, -1, -1, -1, 0, 1, 1, 1}, dx = {0, -1, -1, 0, 1, 1, 1, 0, -1};
-    private static boolean[][] isDisappeared;
-    private static Queue<int[]> clouds, copyPos;
+    private static int[][] map;
+    private static final int[] dy = {0, 0,-1,-1,-1,0,1,1,1}, dx = {0, -1,-1,0,1,1,1,0,-1};
+    private static ArrayDeque<int[]> clouds;
+    private static boolean[][] disappeared;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(stk.nextToken());        
-        m = Integer.parseInt(stk.nextToken());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
 
-        A = new int[n][n];
-        for(int i=0 ; i<n ; i++){
-            stk = new StringTokenizer(br.readLine());
-            for(int j=0 ; j<n ; j++){
-                A[i][j] = Integer.parseInt(stk.nextToken());
+        map = new int[n + 1][n + 1];
+        for(int i=1 ; i<=n ; i++){
+            tokenizer = new StringTokenizer(br.readLine());
+
+            for(int j=1 ; j<=n ; j++){
+                map[i][j] = Integer.parseInt(tokenizer.nextToken());   
             }
         }
 
-        clouds = new LinkedList<>();
-        clouds.add(new int[]{n - 1, 0});
-        clouds.add(new int[]{n - 2, 0});
+        clouds = new ArrayDeque<>();
+        clouds.add(new int[]{n, 1});
+        clouds.add(new int[]{n, 2});
         clouds.add(new int[]{n - 1, 1});
-        clouds.add(new int[]{n - 2, 1});
-
-        copyPos = new LinkedList<>();
+        clouds.add(new int[]{n - 1, 2});
+        
 
         while(m-- > 0){
-            stk = new StringTokenizer(br.readLine());
-            isDisappeared = new boolean[n][n];
-            int d = Integer.parseInt(stk.nextToken()), s = Integer.parseInt(stk.nextToken());
-            raining(d, s);
-            createCloud();
+            tokenizer = new StringTokenizer(br.readLine());
+
+            int d = Integer.parseInt(tokenizer.nextToken());
+            int s = Integer.parseInt(tokenizer.nextToken());
+
+            disappeared = new boolean[n + 1][n + 1];
+
+            moveClouds(d, s);
+            copyWater();
+            makeCloud();
         }
 
         int answer = 0;
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                answer += A[i][j];
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n ; j++){
+                answer += map[i][j];
             }
         }
 
         System.out.println(answer);
     }
 
-    private static int[] moveCloud(int y, int x, int d, int s){
-        int ny = y, nx = x;
+    private static void moveClouds(int d, int s){
+        int size = clouds.size();
 
-        for(int i=0 ; i<s ; i++){
-            ny += dy[d];
-            nx += dx[d];
+        while(size-- > 0){
+            int[] pos = clouds.poll();
+            int y = pos[0], x = pos[1];
 
-            if(ny < 0)ny = n - 1;
-            if(nx < 0)nx = n - 1;
+            int ny = calculatePosition(y, dy[d] * s);
+            int nx = calculatePosition(x, dx[d] * s);
 
-            ny %= n;
-            nx %= n;
+            if(disappeared[ny][nx])
+                continue;
+
+            map[ny][nx]++;
+            clouds.add(new int[]{ny, nx});
+            disappeared[ny][nx] = true;
+        }
+    }
+    
+    private static int calculatePosition(int p, int dist){
+        if(dist == 0)
+            return p;
+
+        int np = p + dist;
+
+        if(np > 0 && np % n == 0){
+            return n;
+        }
+        if(np > 0){
+            return (np % n);
         }
 
-        return new int[]{ny, nx};
+        return n + (np % n);
     }
 
-    private static void raining(int d, int s){
+    private static void copyWater(){
         while(!clouds.isEmpty()){
             int[] cloud = clouds.poll();
             int y = cloud[0], x = cloud[1];
 
-            int[] next = moveCloud(y, x, d, s);
-            int ny = next[0], nx = next[1];
+            int ret = 0;
 
-            A[ny][nx]++;
-            isDisappeared[ny][nx] = true;
-            copyPos.add(new int[]{ny, nx});
-        }
+            for(int i=2 ; i<=8 ; i += 2){
+                int ny = y + dy[i], nx = x + dx[i];
 
-        while(!copyPos.isEmpty()){
-            int[] cur = copyPos.poll();
-            int y = cur[0], x = cur[1];
+                if(1 > ny || ny > n || 1 > nx || nx > n || map[ny][nx] == 0)
+                    continue;
 
-            A[y][x] += waterCopy(y, x);
+                ret++;
+            }
+
+            map[y][x] += ret;
         }
     }
 
-    private static int waterCopy(int y, int x){
-        int ret = 0;
+    private static void makeCloud(){
+        for(int y=1 ; y<=n ; y++){
+            for(int x=1 ; x<=n ; x++){
+                if(disappeared[y][x] || map[y][x] < 2)
+                    continue;
 
-        for(int i = 2 ; i <= 8 ; i += 2){
-            int ny = y + dy[i], nx = x + dx[i];
-            if(0 > ny || ny >= n || 0 > nx || nx >= n || A[ny][nx] == 0)continue;
-            ret++;
-        }
-
-        return ret;
-    }
-
-    private static void createCloud(){
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(isDisappeared[i][j] || A[i][j] < 2)continue;
-                clouds.add(new int[]{i, j});
-                A[i][j] -= 2;
+                map[y][x] -= 2; 
+                clouds.add(new int[]{y, x});
             }
         }
     }
