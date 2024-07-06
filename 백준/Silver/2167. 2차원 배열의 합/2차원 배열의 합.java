fcfa769a9bb9f02b1b1a8d47990c9241b35caf84
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-07-06 12:15:48 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 292 ms, Memory: 27796 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,39 +1,55 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] size = br.readLine().split(" ");
-		int n = Integer.parseInt(size[0]), m = Integer.parseInt(size[1]);
-		int[][] arr = new int[n+1][m+1], sum = new int[n+1][m+1];
-		
-		for(int i=1 ; i<=n ; i++) {
-			String[] values = br.readLine().split(" ");
-			for(int j=0 ; j<m ; j++) {
-				arr[i][j+1] = Integer.parseInt(values[j]);
-			}
-		}
-		
-		for(int i=1 ; i<=n ; i++) {
-			for(int j=1 ; j<=m ; j++) {
-				sum[i][j] += sum[i-1][j] + arr[i][j] - sum[i-1][j-1] + sum[i][j-1];
-			}
-		}
-		
-		int T = Integer.parseInt(br.readLine());
-		while(T-- > 0) {
-			String[] str = br.readLine().split(" ");
-			int sy = Integer.parseInt(str[0]), sx = Integer.parseInt(str[1]),
-					ey = Integer.parseInt(str[2]), ex = Integer.parseInt(str[3]);
-			System.out.println(sum[ey][ex] - sum[sy-1][ex] -  sum[ey][sx-1] + sum[sy-1][sx-1]);
-			
-		}
-		
-		br.close();
-	}
-	
-	
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+
+        int[][] arr = new int[n + 1][m + 1];
+        int[][] prefixSum = new int[n + 1][m + 1];
+
+        for(int i=1 ; i<=n ; i++){
+            tokenizer = new StringTokenizer(br.readLine());
+
+            for(int j=1 ; j<=m ; j++){
+                arr[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
+        }
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=m ; j++){
+                prefixSum[i][j] = arr[i][j] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+            }
+        }
+
+        StringBuilder answer = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        while(T-- > 0){
+            tokenizer = new StringTokenizer(br.readLine());
+
+            int y1 = Integer.parseInt(tokenizer.nextToken());
+            int x1 = Integer.parseInt(tokenizer.nextToken());
+            int y2 = Integer.parseInt(tokenizer.nextToken());
+            int x2 = Integer.parseInt(tokenizer.nextToken());
+
+            int rangeSum = prefixSum[y2][x2] - prefixSum[y2][x1-1] - prefixSum[y1-1][x2] + prefixSum[y1-1][x1-1];
+            answer.append(rangeSum).append("\n");
+        }
+
+
+        bw.write(answer.toString());
+        bw.close();
+    }
 }
