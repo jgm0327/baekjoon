75c0daceae226e134dd96c5225c7f4c09c0fa425
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-07-11 13:31:49 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.19 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,37 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = bfs(x, y, n);
-        return answer;
+        return bfs(x, y, n);
     }
     
-    private int bfs(int start, int end, int n){
-        if(start == end)
-            return 0;
+    private int bfs(int x, int y, int n){
+        if(x == y)return 0;
         
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{start, 0});
-        
+        ArrayDeque<int[]> que = new ArrayDeque<>();
         Map<Integer, Boolean> visit = new HashMap<>();
-        visit.put(start, true);
         
-        final int[][] dx = {{1, n}, {2, 0}, {3, 0}};
+        visit.put(x, true);
+        que.add(new int[]{x, 0});
+        
+        final int[][] dx = {{2,0},{3,0},{1,n}};
         
         while(!que.isEmpty()){
             int[] cur = que.poll();
-            int num = cur[0], dist = cur[1];
+            
+            int curX = cur[0], dist = cur[1];
             
             for(int i=0 ; i<3 ; i++){
-                int nextNum = num * dx[i][0] + dx[i][1];
+                int nx = curX * dx[i][0] + dx[i][1];
                 
-                if(nextNum == end)
+                if(nx == y){
                     return dist + 1;
+                }
                 
-                if(visit.containsKey(nextNum) || nextNum > end)
+                if(nx > y || visit.containsKey(nx)){
                     continue;
+                }
                 
-                que.add(new int[]{nextNum, dist + 1});
-                visit.put(nextNum, true);
+                visit.put(nx, true);
+                que.add(new int[]{nx, dist + 1});
             }
         }
         
