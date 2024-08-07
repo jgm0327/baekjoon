a6a7bb6f00718a64ef56cae69dfc5b6e1ca65bf1
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-07 17:05:40 +0900

    [Silver II] Title: 특별한 마법 공격, Time: 280 ms, Memory: 29468 KB -BaekjoonHub

--- 백준/Silver/31670. 특별한 마법 공격/특별한 마법 공격.java
+++ 백준/Silver/31670. 특별한 마법 공격/특별한 마법 공격.java
@@ -1,30 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][2];
+        int[] r = new int[n + 1];
 
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-
-        int[] numbers = new int[n + 1];
         for(int i=1 ; i<=n ; i++){
-            numbers[i] = Integer.parseInt(tokenizer.nextToken());
+            r[i] = Integer.parseInt(tokenizer.nextToken());
         }
 
-        long[][] dp = new long[n + 1][2];
-        dp[1][0] = 0;
-        dp[1][1] = numbers[1];
+        dp[1][0] = r[0];
 
-        for(int i=2 ; i<=n ; i++){
-            dp[i][0] = dp[i-1][1];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i-1][1]) + numbers[i];
+        for(int i=1 ; i<=n ; i++){
+            dp[i][0] = Math.min(dp[i-1][0], dp[i-1][1]) + r[i];
+            dp[i][1] = dp[i-1][0];
         }
+        
+        bw.write(String.valueOf(Math.min(dp[n][0], dp[n][1])));
 
-        System.out.println(Math.min(dp[n][0], dp[n][1]));
+        bw.flush();
+
+        bw.close();
+        br.close();
     }
 }
