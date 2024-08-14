c610c6bc79b3e7d5c3cdb47588e0ba5fefc3d0b2
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-14 15:05:51 +0900

    [Gold II] Title: 나누기, Time: 248 ms, Memory: 24764 KB, Score: 7 point -BaekjoonHub

---
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static class ShortCut implements Comparable<ShortCut>{
+        int start, end, len, totalDist;
+
+        public ShortCut(int start, int end, int len){
+            this.start = start;
+            this.end = end;
+            this.len = len;
+            totalDist = start;
+        }
+
+        @Override
+        public int compareTo(ShortCut o){
+            if(o.start != this.start)return this.start - o.start;
+            return o.len - this.len;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =Integer.parseInt(br.readLine());
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] prefixSum = new int[n + 1];
+        for(int i=0 ; i<n ; i++){
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+
+            prefixSum[i + 1] = prefixSum[i] + arr[i];
+        }
+
+        long answer = solution(prefixSum, n);
+
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+
+        br.close();
+    }
+
+    private static long solution(final int[] prefixSum, int n){
+        if(prefixSum[n] % 4 != 0)
+            return 0;
+
+
+        if(prefixSum[n] == 0){
+            int cnt = 0;
+            for(int i=1 ; i<prefixSum.length ; i++){
+                if(prefixSum[i] == 0)
+                    cnt++;
+            }
+
+            return combination(cnt);
+        }
+
+        int k = prefixSum[n] / 4;
+        long[] dp = new long[5];
+
+        dp[0] = 1;
+        for(int i=1 ; i<=n ; i++){
+            int t = prefixSum[i] / k;
+
+            if(prefixSum[i] % k != 0 || t < 1 || t > 4)continue;
+
+            dp[t] += dp[t - 1];
+        }
+
+        return dp[4];
+    }
+
+    private static int combination(int n){
+        if(n < 3)return 0;
+
+        return (n * (n - 1) * (n - 2)) / 6;
+    }
+
+}
