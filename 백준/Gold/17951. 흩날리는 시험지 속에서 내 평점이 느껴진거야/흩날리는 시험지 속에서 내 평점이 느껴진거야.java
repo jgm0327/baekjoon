3652ee78df93a0b607c3381586cc3c9ebd4b8c82
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-18 16:55:47 +0900

    [Gold III] Title: 흩날리는 시험지 속에서 내 평점이 느껴진거야, Time: 268 ms, Memory: 23364 KB -BaekjoonHub

---
+++ 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int k = Integer.parseInt(tokenizer.nextToken());
+
+        int[] scores = new int[n];
+        tokenizer = new StringTokenizer(br.readLine());
+
+        for(int i=0 ; i<n ; i++){
+            scores[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        int left = 0, right = 1000_000_000;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            int count = 0, totalScore = 0;
+            for(int score : scores){
+                totalScore += score;
+
+                if(totalScore >= mid){
+                    count++;
+                    totalScore = 0;
+                }
+            }
+
+            if(count >= k){
+                left = mid + 1;
+                
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(right);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
