e199d52e0b510ca55a395655ad4c0b958039b0d9
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-20 11:07:05 +0900

    [Gold II] Title: 컵라면, Time: 944 ms, Memory: 76976 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -5,7 +5,6 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
@@ -21,64 +20,27 @@ class Main {
         }
 
         Arrays.sort(problems, (o1, o2) -> {
-            if(o1[0] != o2[0]) return o1[0] - o2[0];
+            if(o1[0] != o2[0])return o1[0] - o2[0];
             return o2[1] - o1[1];
         });
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> 
-        {
-            if(o1[1] != o2[1])return o1[1] - o2[1];
-            return o1[0] - o2[0];
-        });
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+    
+        int answer = 0;
         for(int[] problem : problems){
             int deadLine = problem[0], count = problem[1];
-
-            if(pq.size() >= deadLine){
-                if(pq.peek()[1] >= count)
-                    continue;
-
-                pq.poll();
+            
+            while(pq.size() >= deadLine && pq.peek()[1] < count){
+                answer -= pq.poll()[1];
             }
 
+            if(pq.size() == deadLine)continue;
+
+            answer += count;
             pq.add(problem);
         }
 
-        int answer = 0;
-        while(!pq.isEmpty()){
-            answer += pq.poll()[1];
-        }
-
-        bw.write(String.valueOf(answer));
-
-        /**
-         * 3
-         * 3 9
-         * 3 4
-         * 1 1
-         * 
-         * (1, 1), (3, 9), (3, 4)
-         * 
-         * 
-         * 4
-         * 1 1
-         * 2 1
-         * 3 10
-         * 3 10
-         * 
-         * (1, 1), (3, 10), (3, 10)
-         * 
-         * 4
-         * 1 50
-         * 2 1
-         * 3 60
-         * 3 70
-         * 
-         * (1, 50), (3, 60), (4, 70)
-         */
-
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
 
         br.close();
     }
