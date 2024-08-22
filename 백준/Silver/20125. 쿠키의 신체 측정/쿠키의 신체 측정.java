a6252f5d8a8c069b433ea3362b10e5f28d93cea6
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-22 11:16:29 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 244 ms, Memory: 21456 KB -BaekjoonHub

--- 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
@@ -3,12 +3,15 @@ import java.util.*;
 
 class Main {
 
+    static char[][] board;
+    static int n;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        char[][] board = new char[n][n];
-        int[] heartPosition = {-1,-1};
+        n = Integer.parseInt(br.readLine());
+        board = new char[n][n];
+        int[] start = {-1,-1};
         boolean findHead = false;
 
         for(int i=0 ; i<n ; i++){
@@ -19,37 +22,37 @@ class Main {
 
                 if(board[i][j] == '*' && !findHead){
                     findHead = true;
-                    heartPosition = new int[]{i+1, j};
+                    start = new int[]{i+1, j};
                 }
             }
         }
 
-        int leftArmLength = getLength(board, -1,0, heartPosition);
-        int rightArmLength = getLength(board, 1, 0, heartPosition);
-        int waistLength = getLength(board, 0, 1, heartPosition);
-        int leftLegLength = getLength(board, 0, 1, new int[]{heartPosition[0] + waistLength, heartPosition[1] - 1});
-        int rightLegLength = getLength(board, 0, 1, new int[]{heartPosition[0] + waistLength, heartPosition[1] + 1});
+        int leftArm = getLength(-1,0, start);
+        int rightArm = getLength(1, 0, start);
+        int waist = getLength(0, 1, start);
+        int leftLeg = getLength(0, 1, new int[]{start[0] + waist, start[1] - 1});
+        int rightLeg = getLength(0, 1, new int[]{start[0] + waist, start[1] + 1});
 
         StringBuilder answer = new StringBuilder();
-        answer.append(heartPosition[0] + 1).append(" ").append(heartPosition[1] + 1).append("\n");
+        answer.append(start[0] + 1).append(" ").append(start[1] + 1).append("\n");
 
-        answer.append(leftArmLength).append(" ").append(rightArmLength).append(" ").append(waistLength)
-        .append(" ").append(leftLegLength).append(" ").append(rightLegLength);
+        answer.append(leftArm).append(" ").append(rightArm).append(" ").append(waist)
+        .append(" ").append(leftLeg).append(" ").append(rightLeg);
 
         System.out.println(answer);
 
         br.close();
     }
 
-    private static int getLength(char[][] board, int dx, int dy, int[] heartPosition){
-        int len = 0, y = heartPosition[0]  + dy, x = heartPosition[1] + dx;
+    private static int getLength(int dx, int dy, int[] start){
+        int len = 0, y = start[0]  + dy, x = start[1] + dx;
 
         while(board[y][x] == '*'){
             len++;
             y += dy;
             x += dx;
 
-            if(y >= board.length || x >= board.length || y < 0 || x < 0)
+            if(y >= n || x >= n || y < 0 || x < 0)
                 break;
         }
 
