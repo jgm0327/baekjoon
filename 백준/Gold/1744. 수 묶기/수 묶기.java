0dd98d749a25286cf59bdfd7f525396d607b36d3
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-08-31 12:47:23 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,45 +1,44 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
-class Main{
-    public static void main(String... args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> positive = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        PriorityQueue<Integer> negative = new PriorityQueue<>();
 
-        for(int i=0 ; i<n ; i++){
-            int num = Integer.parseInt(br.readLine());
-            if(num <= 0)negative.add(num);
-            else positive.add(num);
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
         }
 
-        int answer = 0;
-        while(!positive.isEmpty()){
-            int num1 = positive.poll();
-            if(positive.isEmpty()){
-                answer += num1;
-                break;
+        Arrays.sort(numbers);
+
+        int answer = 0, idx1 = n - 1;
+
+        while(idx1 > 0 && numbers[idx1] > 0){
+            if(numbers[idx1] == 1 
+            || (idx1 > 0 && (numbers[idx1 - 1] <= 1))){
+                answer += numbers[idx1--];
             }
-            int num2 = positive.poll();
-            if(num1 == 1 || num2 == 1)answer += (num1 + num2);
-            else answer += (num1 * num2);
-        }
-
-        while(!negative.isEmpty()){
-            int num1 = negative.poll();
-            if(negative.isEmpty()){
-                answer += num1;
-                break;
+            else if(idx1 > 0){
+                answer += numbers[idx1] * numbers[idx1 - 1];
+                idx1 -= 2;
             }
-            int num2 = negative.poll();
-            answer += (num1 * num2);
         }
 
+        int idx2 = 0;
+        while(idx2 <= idx1){
+            if(idx2 + 1 <= idx1){
+                answer += numbers[idx2] * numbers[idx2 + 1];
+                idx2 += 2;
+            }
+            else answer += numbers[idx2++];
+        }
 
-        System.out.println(answer);
+        bw.write(String.valueOf(answer));
+        bw.close();
         br.close();
     }
 }
