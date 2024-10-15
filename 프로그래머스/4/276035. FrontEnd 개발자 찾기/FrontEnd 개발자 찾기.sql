80140a580a1b101dd4a267935cdd2465858a0e80
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-10-15 16:38:59 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,8 +1,7 @@
 -- 코드를 작성해주세요
-with
-front as (select sum(code) as 'CODE' from skillcodes where category = 'Front End')
-
-select d.id, d.email, d.first_name, d.last_name
-from developers d, front f
-where d.skill_code & f.CODE != 0
-order by d.id;
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES
+    WHERE CATEGORY = 'Front End') != 0
+    
+ORDER BY 1
