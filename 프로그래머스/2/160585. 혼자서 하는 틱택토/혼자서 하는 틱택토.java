da1696ae06180e16fea827bbbfeac0c7a0da3c1f
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-10-17 14:08:59 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 83.79 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,84 +1,94 @@
 class Solution {
-    int answer = 0;
     public int solution(String[] board) {
-        char[][] pattern = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
-        
-        dfs(0, pattern, board);
-        return answer;
+        int answer = -1;
+        char[][] temp = {{'.','.','.'},{'.','.','.'},{'.','.','.'}};
+        return dfs(0, board, temp);
     }
     
-    private boolean isOver(char[][] pattern){
+    private int dfs(int depth, String[] board, char[][] temp){
+        if(correctBoard(board, temp)){
+            return 1;
+        }
+        
+        if(gameOver(temp))
+            return 0;
+        
+        if(depth == 9){
+            return 0;
+        }
+        
+        char shape = 'O';
+        if(depth % 2 == 1)shape = 'X';
         
         for(int i=0 ; i<3 ; i++){
-            int oCnt1 = 0, xCnt1 = 0, oCnt2 = 0, xCnt2 = 0;
+            for(int j=0 ; j<3; j++){
+                if(temp[i][j] != '.')
+                    continue;
+                
+                temp[i][j] = shape;
+                if(dfs(depth + 1, board, temp) == 1){
+                    return 1;
+                }
+                temp[i][j] = '.';
+            }
+        }
+        
+        return 0;
+    }
+    
+    private boolean gameOver(char[][] temp){
+        for(int i=0 ; i<3 ; i++){
+            char shape = temp[i][0];
+            int count = 0;
+            
+            if(shape == '.')
+                continue;
             
             for(int j=0 ; j<3 ; j++){
-                char shape1 = pattern[i][j];
-                char shape2 = pattern[j][i];
+                if(shape != temp[i][j])
+                    break;
                 
-                if(shape1 == 'O')oCnt1++;
-                else if(shape1 == 'X')xCnt1++;
-                
-                if(shape2 == 'O')oCnt2++;
-                else if(shape2 == 'X')xCnt2++;
+                count++;
             }
             
-            if(oCnt1 == 3 || xCnt2 == 3 || xCnt1 == 3 || oCnt2 == 3)
+            if(count == 3)
                 return true;
         }
         
-        int oCnt1 = 0, xCnt1 = 0, oCnt2 = 0, xCnt2 = 0;
+        for(int j=0 ; j<3 ; j++){
+            char shape = temp[0][j];
+            int count = 0;
+            
+            if(shape == '.')
+                continue;
+            
             for(int i=0 ; i<3 ; i++){
-                char shape1 = pattern[i][i];
-                char shape2 = pattern[i][2 - i];
+                if(shape != temp[i][j])
+                    break;
                 
-                if(shape1 == 'O')oCnt1++;
-                else if(shape1 == 'X')xCnt1++;
-                
-                if(shape2 == 'O')oCnt2++;
-                else if(shape2 == 'X')xCnt2++;
+                count++;
             }
-        
-        
-            if(oCnt1 == 3 || xCnt2 == 3 || xCnt1 == 3 || oCnt2 == 3)
+            
+            if(count == 3)
                 return true;
+        }
+        
+        if(temp[0][0] != '.' && temp[0][0] == temp[1][1] && temp[1][1] == temp[2][2])
+            return true;
+        
+        if(temp[0][2] != '.' && temp[0][2] == temp[1][1] && temp[1][1] == temp[2][0])
+            return true;
         
         return false;
     }
     
-    private void dfs(int depth, char[][] pattern, final String[] board){
-        if(check(board, pattern)){
-            answer = 1;
-            return;
-        }
-        
-        if(isOver(pattern))
-            return;
-        
-        char shape = 'O';
-        if(depth % 2 == 1)
-            shape = 'X';
-        
+    private boolean correctBoard(String[] board, char[][] temp){
         for(int i=0 ; i<3 ; i++){
             for(int j=0 ; j<3 ; j++){
-                if(pattern[i][j] != '.')
+                if(board[i].charAt(j) == temp[i][j])
                     continue;
                 
-                pattern[i][j] = shape;
-                
-                dfs(depth + 1, pattern, board);
-                if(answer == 1)return;
-                
-                pattern[i][j] = '.';
-            }
-        }
-    }
-    
-    private boolean check(String[] board, char[][] pattern){
-        for(int i=0 ; i<3 ; i++){
-            for(int j=0 ; j<3 ; j++){
-                if(board[i].charAt(j) != pattern[i][j])
-                    return false;
+                return false;
             }
         }
         
