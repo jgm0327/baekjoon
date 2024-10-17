e399379058231e022653f3f3fec631cd47d2bbf8
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-10-17 15:50:01 +0900

    [level 2] Title: 시소 짝꿍, Time: 91.85 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -3,37 +3,40 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        Map<Long, Long> count = new HashMap<>();
+        Map<Integer, Long> ori = new HashMap<>();
         
+        boolean[][] visit = new boolean[1001][1001];
         for(int weight : weights){
-            count.put((long)weight, count.getOrDefault((long)weight, 0L) + 1L);
+            ori.put(weight, ori.getOrDefault(weight, 0L) + 1);
         }
         
-        List<Long> list = new ArrayList<>();
-        for(long key : count.keySet()){
-            long value = count.get(key);
-            answer += (value * (value - 1)) / 2;
-            list.add((long)key);
-        }
-        
-        final long[][] mul = {{2L, 3L}, {3L, 2L}, {2L, 4L}, {4L, 2L}, {3L, 4L}, {4L, 3L}};
-        
-        System.out.println(list);
-        for(int i=0 ; i<list.size() ; i++){
-            long num1 = list.get(i);
-            for(int j=i+1 ; j<list.size() ; j++){
-                long num2 = list.get(j);
+        for(int o1 : ori.keySet()){
+            answer += (ori.get(o1) * (ori.get(o1) - 1)) / 2;
+            visit[o1][o1] = true;
+            
+            for(int o2 : ori.keySet()){
+                if(visit[o1][o2])
+                    continue;
                 
-                for(long[] d : mul){
-                    if(num1 * d[0] != num2 * d[1])
-                        continue;
-                    
-                    answer += (count.get(num2) * count.get(num1));
-                    break;
+                if(canRide(o1, o2)){
+                    answer += ori.get(o1) * ori.get(o2);
+                    visit[o1][o2] = visit[o2][o1] = true;
                 }
             }
         }
-
+        
         return answer;
+    }
+    
+    private boolean canRide(int o1, int o2){
+        for(int i=2 ; i<=4 ; i++){
+            for(int j=2 ; j<=4 ; j++){
+                if(o1 * i == o2 * j){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
     }
 }
