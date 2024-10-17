e754d74cfa15098cf7dfe77fd6093c50dd4a9d84
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-10-17 13:44:03 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,63 +2,69 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] board) {
-        int n = board.length, m = board[0].length();
-        int startY = -1, startX = -1;
+        int[] start = {-1,-1};
+        int[] end = {-1,-1};
         
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<m ; j++){
-                if(board[i].charAt(j) != 'R')
-                    continue;
-                startY = i;
-                startX = j;
+        for(int row = 0 ; row < board.length ; row++){
+            for(int col = 0 ; col < board[0].length() ; col++){
+                char ch = board[row].charAt(col);
+                
+                if(ch == 'R')start = new int[]{row, col};
+                if(ch == 'G')end = new int[]{row, col};
             }
         }
         
-        return bfs(startY, startX, n, m, board);
+        return bfs(start, end, board);
     }
     
-    private int bfs(int startY, int startX, int n, int m, final String[] board){
-        Queue<int[]> que = new LinkedList<>();
-        que.add(new int[]{startY, startX, 0});
-        
-        boolean[][] visit = new boolean[n][m];
-        visit[startY][startX] = true;
+    private int bfs(int[] start, int[] end, String[] board){
+        Queue<int[]> que = new ArrayDeque<>();
+        que.add(new int[]{start[0], start[1], 0});
         
         final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
+        boolean[][][] visit = new boolean[board.length][board[0].length()][4];
         
+        Arrays.fill(visit[start[0]][start[1]], true);
+        
+        int ret = Integer.MAX_VALUE;
         while(!que.isEmpty()){
             int[] cur = que.poll();
-            int y = cur[0], x = cur[1], dist = cur[2];
+            int y = cur[0], x = cur[1], time = cur[2];
             
             for(int i=0 ; i<4 ; i++){
-                int[] next = getPosition(n, m, y, x, dy[i], dx[i], board);
-                int ny = next[0], nx = next[1];
+                int[] next = move(dy[i], dx[i], y, x, board);
                 
-                if(visit[ny][nx])
+                int ny = next[0], nx  = next[1];
+                
+                if(visit[ny][nx][i])
                     continue;
                 
-                if(board[ny].charAt(nx) == 'G')
-                    return dist + 1;
+                if(ny == end[0] && nx == end[1]){
+                    ret = Math.min(time + 1, ret);
+                    continue;
+                }
+                    
                 
-                que.add(new int[]{ny, nx, dist + 1});
-                visit[ny][nx] = true;
+                visit[ny][nx][i] = true;
+                que.add(new int[]{ny, nx, time + 1});
             }
         }
         
-        return -1;
+        return ret == Integer.MAX_VALUE ? -1 : ret;
     }
     
-    private int[] getPosition(int n, int m, int y, int x, int dy, int dx, final String[] board){
-        int end = Math.max(n, m);
+    private int[] move(int dy, int dx, int y, int x, String[] board){
+        int ny = y + dy, nx = x + dx;
         
-        for(int i=1 ; i<=end ; i++){
-            int ny = y + dy * i, nx = x + dx * i;
-            
-            if(0 > ny || ny >= n || 0 > nx || nx >= m 
-               || board[ny].charAt(nx) == 'D')
+        while(0 <= ny && ny < board.length && 0 <= nx && nx < board[0].length()){
+            if(board[ny].charAt(nx) == 'D'){
                 return new int[]{ny - dy, nx - dx};
+            }
+            
+            ny += dy;
+            nx += dx;
         }
         
-        return new int[]{y, x};
+        return new int[]{ny - dy, nx - dx};
     }
 }
