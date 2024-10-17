df59bbe736294b5b2e90bb0286fc4afe9d55d99f
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-10-17 14:59:23 +0900

    [level 2] Title: 숫자 변환하기, Time: 47.53 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,37 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-        return bfs(x, y, n);
-    }
-    
-    private int bfs(int x, int y, int n){
-        if(x == y)return 0;
+        if(y == x)
+            return 0;
         
-        ArrayDeque<int[]> que = new ArrayDeque<>();
-        Map<Integer, Boolean> visit = new HashMap<>();
+        Queue<int[]> que = new ArrayDeque<>();
+        int[][] dx = {{2, 0}, {3, 0}, {1, n}};
         
-        visit.put(x, true);
         que.add(new int[]{x, 0});
-        
-        final int[][] dx = {{2,0},{3,0},{1,n}};
+        boolean[] visit = new boolean[1000_001];
+        visit[x] = true;
         
         while(!que.isEmpty()){
             int[] cur = que.poll();
             
-            int curX = cur[0], dist = cur[1];
-            
+            int cx = cur[0], dist = cur[1];
             for(int i=0 ; i<3 ; i++){
-                int nx = curX * dx[i][0] + dx[i][1];
+                int nx = cx * dx[i][0] + dx[i][1];
                 
-                if(nx == y){
-                    return dist + 1;
-                }
-                
-                if(nx > y || visit.containsKey(nx)){
+                if(nx > y || visit[nx])
                     continue;
-                }
                 
-                visit.put(nx, true);
+                if(nx == y)
+                    return dist + 1;
+                
+                visit[nx] = true;
                 que.add(new int[]{nx, dist + 1});
             }
         }
