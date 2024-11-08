2daf58ee8043494b0f7c7204d4f77e507e7ede8c
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-11-08 16:03:22 +0900

    [Gold III] Title: 게리맨더링 2, Time: 320 ms, Memory: 30320 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
@@ -2,35 +2,31 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-
-    private static int n, answer, total;
-    private static int[][] board;
-    private static boolean[][] isLine;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        board = new int[n + 1][n + 1];
-
-        for(int i=1 ; i<=n ; i++){
+        int[][] board = new int[n][n];
+        int total = 0;
+        for(int i=0 ; i<n ; i++){
             StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-            for(int j=1 ; j<=n ; j++){
+            for(int j=0 ; j<n ; j++){
                 board[i][j] = Integer.parseInt(tokenizer.nextToken());
                 total += board[i][j];
             }
         }
 
-        // System.out.println(total);
-
-        answer = Integer.MAX_VALUE;
-
-        for(int y=1 ; y<=n ; y++){
-            for(int x=1 ; x<=n ; x++){
-                solution(y, x);
+        int answer = Integer.MAX_VALUE;
+        for(int y = 0 ; y < n ; y++){
+            for(int x = 0 ; x < n ; x++){
+                for(int d1 = 1 ; d1 < n ; d1++){
+                    for(int d2 = 1 ; d2 < n ; d2++){
+                        answer = Math.min(answer, getMinDiff(board, y, x, d1, d2, total, n));
+                    }
+                }
             }
         }
 
@@ -39,110 +35,104 @@ class Main {
         br.close();
     }
 
-    private static void solution(int y, int x){
-        for(int d1=1 ; d1<=n ; d1++){
-            for(int d2=1 ; d2<=n ; d2++){
-                if(!isIn(y-d1, x+d1) || !isIn(y+d2, x+d2) || !isIn(y-d1+d2, x+d1+d2) || !isIn(y+d1-d2, x+d1+d2))
-                    continue;
+    private static int getMinDiff(final int[][] board, int y, int x, int d1, int d2, int total, int n){
+        if(y + d1 >= n || y + d2 >= n || y + d1 + d2 >= n
+        || x - d1 < 0 || x + d2 >= n || x-d1+d2 >= n || x+d2-d1 >= n
+        || x+d2-d1 < 0)
+            return Integer.MAX_VALUE;
 
-                isLine = new boolean[n + 1][n + 1];
-                answer = Math.min(answer, getMinDiff(y, x, d1, d2));
-            }
-        }
-    }
+        int minValue = Integer.MAX_VALUE, maxValue = Integer.MIN_VALUE;
+        boolean[][] walls = setWall(board, y, x, d1, d2, n);
 
-    private static boolean isIn(int y, int x){
-        return 1<=y && y<=n && 1<=x && x<=n;
-    }
-
-    private static int getMinDiff(int y, int x, int d1, int d2){
-        int[] sum = new int[5];
-        int remain = 0;
-
-        setLine(y, x, d1, d2);
-
-        //1
-        for(int i=1 ; i<y ; i++){
-            for(int j=1 ; j<=x+d1;j++){
-                if(isLine[i][j])
+        int sum = 0;
+        for(int i=0 ; i<y+d1 ; i++){
+            for(int j=0 ; j<=x ; j++){
+                if(walls[i][j])
                     break;
 
-                sum[0] += board[i][j];
+                sum += board[i][j];
             }
         }
-        remain += sum[0];
 
-        //2
-        for(int i=1 ; i<=y+d2-d1 ; i++){
-            for(int j=n ; j>x+d1 ;j--){
-                if(isLine[i][j])
+        if(sum == 0)
+            return Integer.MAX_VALUE;
+
+        total -= sum;
+        maxValue = Math.max(maxValue, sum);
+        minValue = Math.min(minValue, sum);
+
+        sum = 0;
+        for(int i=0 ; i<=y+d2 ; i++){
+            for(int j=n-1 ; j>x ; j--){
+                if(walls[i][j])
                     break;
 
-                // print(y, x, d1, d2, i, j, 0);
-                sum[1] += board[i][j];
+                sum += board[i][j];
             }
         }
-        // print(y, x, d1, d2, -1, -1, sum[1]);
-        remain += sum[1];
+        
+        if(sum == 0)
+            return Integer.MAX_VALUE;
+            
+        total -= sum;
+        maxValue = Math.max(maxValue, sum);
+        minValue = Math.min(minValue, sum);
 
-        //3
-        for(int i=y ; i<=n ; i++){
-            for(int j=1 ; j<x+d2 ; j++){
-                if(isLine[i][j])
+        sum = 0;
+        for(int i=y+d1 ; i<n ; i++){
+            for(int j=0 ; j<x-d1+d2 ; j++){
+                if(walls[i][j])
                     break;
 
-                sum[2] += board[i][j];
+                sum += board[i][j];
             }
         }
-        remain += sum[2];
+        
+        if(sum == 0)
+            return Integer.MAX_VALUE;
+            
+        total -= sum;
+        maxValue = Math.max(maxValue, sum);
+        minValue = Math.min(minValue, sum);
 
-        //4
-        for(int i=y-d1+d2+1 ; i<=n ; i++){
-            for(int j=n ; j>=x+d2 ;j--){
-                if(isLine[i][j])
+        sum = 0;
+        for(int i=y+d2+1 ; i<n ; i++){
+            for(int j=n-1 ; j>=x-d1+d2 ; j--){
+                if(walls[i][j])
                     break;
-                
-                sum[3] += board[i][j];
-                // print(y, x, d1, d2, i, j, 0);
+                sum += board[i][j];
             }
         }
-        remain += sum[3];
+        
+        if(sum == 0)
+            return Integer.MAX_VALUE;
+            
+        total -= sum;
+        maxValue = Math.max(maxValue, sum);
+        minValue = Math.min(minValue, sum);
 
-        //5
-        sum[4] = total - remain;
-
-        Arrays.sort(sum);
-
-        // System.out.println(y + " " + x + " " + d1 + " " +d2);
-        // System.out.println(sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] + " " + sum[4]);
+        // System.out.println(total);
+        maxValue = Math.max(maxValue, total);
+        minValue = Math.min(minValue, total);
+        // System.out.println("ret: " + (maxValue - minValue));
         // System.out.println();
 
-        return sum[4] - sum[0];
+        return maxValue - minValue;
     }
 
-    private static void print(int y, int x, int d1, int d2, int i, int j, int sum){
-        if(y != 5 || x != 3 || d1 != 2 || d2 != 1)  
-            return;
+    private static boolean[][] setWall(int[][] board, int y, int x, int d1, int d2, int n){
+        boolean[][] wall = new boolean[n][n];
 
-        System.out.println(i + " " + j);
-        System.out.println(sum);
-    }
-
-    private static void setLine(int y, int x, int d1, int d2){
         for(int i=0 ; i<=d1 ; i++){
-            isLine[y-i][x+i] = true;
+            wall[y+i][x-i] = true;
+            wall[y+d2+i][x+d2-i] = true;
         }
 
         for(int i=0 ; i<=d2 ; i++){
-            isLine[y+i][x+i] = true;
+            wall[y+i][x+i] = true;
+            wall[y+d1+i][x-d1+i] = true;
         }
 
-        for(int i=0 ; i<=d2 ; i++){
-            isLine[y-d1+i][x+d1+i] = true;
-        }
-
-        for(int i=0 ; i<=d1 ; i++){
-            isLine[y+d2-i][x+d2+i] = true;
-        }
+        return wall;
     }
 }
