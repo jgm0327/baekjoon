1f0c53382350cc33e26ac3f9dcc0099a03799b9c
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-11-25 13:57:58 +0900

    [Gold II] Title: 레이스, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/1508. 레이스/레이스.java
+++ 백준/Gold/1508. 레이스/레이스.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    // 반례 참고
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,33 +20,27 @@ class Main {
 
         StringBuilder answer = new StringBuilder();
 
-        int left = 0, right = n, maxValue = 0;
+        int left = 0, right = n;
         while(left <= right){
             int mid = (left + right) / 2;
 
-            int total = 1, prev = points[0];
-            List<Integer> indices = new ArrayList<>();
-            indices.add(0);
+            int total = 1, prev = points[0], cnt = 1;
+            StringBuilder comp = new StringBuilder("1");
             for(int i=1 ; i<k ; i++){
+                char c = '0';
                 if(mid < points[i] - prev){
                     total++;
                     prev = points[i];
-                    indices.add(i);
+                    cnt++;
+                    
+                    if(cnt <= m)
+                        c = '1';
                 }
+                comp.append(c);
             }
 
             if(total >= m){
                 left = mid + 1;
-                StringBuilder comp = new StringBuilder();
-                int idx = 0;
-                for(int i=0 ; i<k ; i++){
-                    if(idx < indices.size() && idx < m && indices.get(idx) == i){
-                        idx++;
-                        comp.append("1");
-                        continue;
-                    }
-                    comp.append("0");
-                }
                 answer = comp;
             }
             else
