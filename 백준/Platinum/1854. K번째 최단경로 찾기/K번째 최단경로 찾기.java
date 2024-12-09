217549dae1261c8ac6e125e763ab0722788c764a
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-12-09 14:03:51 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 3716 ms, Memory: 124660 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+        int k = Integer.parseInt(tokenizer.nextToken());
+
+        List<long[]>[] graph = new ArrayList[n + 1];
+        for(int i=0 ; i<=n ; i++)
+            graph[i] = new ArrayList<>();
+
+        while(m-- > 0){
+            tokenizer = new StringTokenizer(br.readLine());
+
+            int sour = Integer.parseInt(tokenizer.nextToken());
+            long des = Integer.parseInt(tokenizer.nextToken());
+            long dist = Integer.parseInt(tokenizer.nextToken());
+
+            graph[sour].add(new long[]{des, dist});
+        }
+
+        bw.write(dijkstra(graph, n, k));
+        bw.close();
+        br.close();
+    }
+
+    private static String dijkstra(final List<long[]>[] graph, int n, int k){
+        PriorityQueue<Long>[] orderCounts = new PriorityQueue[n + 1];
+        for(int i=0 ; i<=n ; i++){
+            orderCounts[i] = new PriorityQueue<>(Collections.reverseOrder());
+        }
+        orderCounts[1].add(0L);
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>((o1, o2) -> {
+            if(o1[1] > o2[1])
+                return 1;
+            else if(o1[1] < o2[1])
+                return -1;
+            return 0;
+        });
+
+        pq.add(new long[]{1, 0});
+
+        long[] costs = new long[n + 1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[1] = 0;
+
+        while(!pq.isEmpty()){
+            long[] cur = pq.poll();
+
+            long sour = cur[0], dist = cur[1];
+            for(long[] des : graph[(int)sour]){
+                long nextDist = dist + des[1];
+                int point = (int)des[0];
+
+                if(orderCounts[point].size() == k 
+                && orderCounts[point].peek() < nextDist)
+                    continue;
+
+                if(orderCounts[point].size() == k)
+                    orderCounts[point].poll();
+
+                costs[point] = nextDist;
+                orderCounts[point].add(nextDist);
+                pq.add(new long[]{point, nextDist});
+            }
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for(int i=1 ; i<=n ; i++){
+            long kthDist = -1;
+            if(orderCounts[i].size() == k)
+                kthDist = orderCounts[i].peek();
+
+            ret.append(kthDist).append('\n');
+        }
+
+        return ret.toString();
+    }
+}
