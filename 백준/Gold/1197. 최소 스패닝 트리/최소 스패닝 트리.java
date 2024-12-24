17c3edbadaf2507bb2f370c4162ab714b53a8265
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2024-12-24 13:01:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 504 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,71 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main{
+class Main {
     private static int n;
     private static int[] parents;
+    private static PriorityQueue<int[]> graph;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(tokenizer.nextToken());
         int m = Integer.parseInt(tokenizer.nextToken());
-        
-        parents = new int[n + 1];
-        for(int i=1 ; i<=n ; i++){
-            parents[i] = i;
-        }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+        graph = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+
         while(m-- > 0){
             tokenizer = new StringTokenizer(br.readLine());
-            int sour, des, weight;
 
-            sour = Integer.parseInt(tokenizer.nextToken());
-            des = Integer.parseInt(tokenizer.nextToken());
-            weight = Integer.parseInt(tokenizer.nextToken());
+            int a = Integer.parseInt(tokenizer.nextToken());
+            int b = Integer.parseInt(tokenizer.nextToken());
+            int c = Integer.parseInt(tokenizer.nextToken());
 
-            pq.add(new int[]{sour, des, weight});
+            graph.add(new int[]{a, b, c});
         }
 
-        System.out.println(mst(pq));
+        parents = new int[n + 1];
+        for(int i=1 ; i<=n ; i++)
+            parents[i] = i;
+
+        bw.write(String.valueOf(MST()));
+        bw.close();
+        br.close();
     }
 
-    private static int mst(PriorityQueue<int[]> pq){   
+    private static int MST(){
         int ret = 0;
+        while(!graph.isEmpty()){
+            int[] cur = graph.poll();
 
-        while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int x = cur[0], y = cur[1], weight = cur[2];
+            int sour = cur[0], des = cur[1], cost = cur[2];
 
-            if(findParent(x) == findParent(y))
+            if(findParent(sour) == findParent(des))
                 continue;
-            
-            union(x, y);
-            ret += weight;
+                
+            ret += cost;
+            union(sour, des);
         }
 
         return ret;
     }
-    
+
     private static int findParent(int x){
         if(x == parents[x])
             return x;
+
         return parents[x] = findParent(parents[x]);
     }
 
-    private static void union(int x, int y){
-        int px = findParent(x), py = findParent(y);
+    private static void union(int y, int x){
+        int py = findParent(y);
+        int px = findParent(x);
 
-        if(px == py)
+        if(py == px)
             return;
 
         parents[py] = px;
     }
-
 }
