f9f43db45eca9efd2c146a7867ae790614f7301f
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-02-16 16:16:30 +0900

    [Silver V] Title: 사과 담기 게임, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -2,31 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main{
+class Main
+{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] size = br.readLine().split(" ");
-        int n = Integer.parseInt(size[0]), m = Integer.parseInt(size[1]);
+        
+        String[] str = br.readLine().split(" ");
+        int m = Integer.parseInt(str[1]);
+
         int j = Integer.parseInt(br.readLine());
 
-        int[] pos = new int[j];
-        for(int i=0 ; i<j ; i++){
-            pos[i] = Integer.parseInt(br.readLine());
-        }
+        int start = 1, end = m;
+        int answer = 0;
+        while(j-- > 0){
+            int number = Integer.parseInt(br.readLine());
 
-        int start = 1, end = m, answer = 0;
-        for(int i=0 ; i<j ; i++){
-            if(start >= pos[i]){
-                answer += (start - pos[i]);
-                start = pos[i];
+            if(start <= number && number <= end)
+                continue;
+
+            if(number < start){
+                answer += start - number;
+                start = number;
                 end = start + m - 1;
-            }else if(end <= pos[i]){
-                answer += (pos[i] - end);
-                end = pos[i];
+            }
+            else{
+                answer += number - end;
+                end = number;
                 start = end - m + 1;
             }
-        }
+        } 
+
         System.out.println(answer);
+
         br.close();
     }
 }
