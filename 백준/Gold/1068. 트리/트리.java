85ea95e9523f026c7f2cf4242bbf0e5f9915c770
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-02-16 19:27:27 +0900

    [Gold V] Title: 트리, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -3,49 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
-	public class Main {
-		private static int n, root, k, answer;
-		private static List<Integer>[] tree;
-		
-		public static void main(String[] args) throws IOException{
-			init();
-			dfs(root);
-			System.out.println(answer);
-		}
-		
-		private static void init() throws IOException{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			n = Integer.parseInt(br.readLine());
-			tree = new ArrayList[n];
-			answer = 0;
-			for(int i=0 ; i<n ; i++)tree[i] = new ArrayList<>();
-			
-			String[] values = br.readLine().split(" ");
-			for(int i=0 ; i<n ; i++) {
-				int parent = Integer.parseInt(values[i]);
-				if(parent == -1) {
-					root = i;
-					continue;
-				}
-				tree[parent].add(i);
-			}
-			k = Integer.parseInt(br.readLine());
-			br.close();
-		}
-		
-		private static void dfs(int parent) {
-			if(parent == k)return;
-			if(tree[parent].isEmpty()) {
-				answer++;
-				return;
-			}
-			boolean flag = true;
-			for(int child : tree[parent]) {
-				if(child == k)continue;
-				flag = false;
-				dfs(child);
-			}
-			if(flag)answer++;
-		}
+class Main {
+
+    static List<Integer>[] tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[n];
+        for(int i=0 ; i<n ; i++){
+            tree[i] = new ArrayList<>();
+        }
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        int root = 0;
+        for(int i=0 ;i<n ; i++){
+            int parent = Integer.parseInt(tokenizer.nextToken());
+            if(parent == -1){
+                root = i;
+                continue;
+            }
+
+            tree[parent].add(i);
+        }
+
+        int removeNode = Integer.parseInt(br.readLine());
+
+        System.out.println(dfs(removeNode, root));
+        
+        br.close();
+    }
+
+    static int dfs(int removeNode, int parent){
+        if(removeNode == parent)
+            return 0;
+
+        int leafNode = 1;
+        int ret = 0;
+
+        for(int child : tree[parent]){
+            if(removeNode == child)
+                continue;
+
+            leafNode = 0;
+            ret += dfs(removeNode, child);
+        }
+
+        return leafNode + ret;
+    }
 }
