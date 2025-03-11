e8427d33640118a33ba7e090e194c5ccd3cc6366
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-03-11 13:23:47 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14684 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,33 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main{
-
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] house = new int[n + 1][3];
 
-        for(int i=1 ; i<=n ; i++){
-            StringTokenizer stk = new StringTokenizer(br.readLine());
-            for(int j=0 ; j<3 ; j++){
-                house[i][j] = Integer.parseInt(stk.nextToken());
-            }
+        int[][] rgb = new int[n][3];
+        for(int i=0 ; i<n ; i++){
+            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+            rgb[i][0] = Integer.parseInt(tokenizer.nextToken());
+            rgb[i][1] = Integer.parseInt(tokenizer.nextToken());
+            rgb[i][2] = Integer.parseInt(tokenizer.nextToken());
         }
 
         int[][] dp = new int[n + 1][3];
 
-        for(int i=1; i<=n ; i++){
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + house[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
-            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + house[i][2];
+        for(int i=0 ; i<n ; i++){
+            dp[i + 1][0] = Math.min(dp[i][1], dp[i][2]) + rgb[i][0];
+            dp[i + 1][1] = Math.min(dp[i][0], dp[i][2]) + rgb[i][1];
+            dp[i + 1][2] = Math.min(dp[i][1], dp[i][0]) + rgb[i][2];
         }
 
-        System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
-    }
+        int answer = Integer.MAX_VALUE;
+        for(int i=0 ; i<3 ; i++){
+            answer = Math.min(dp[n][i], answer);
+        }
 
+        System.out.println(answer);
+        br.close();
+    }
 }
