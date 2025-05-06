fe69d3b8ccde3915a52523b88af7335d19371faf
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-05-06 21:35:21 +0900

    [Gold III] Title: 모양 만들기, Time: 800 ms, Memory: 246244 KB -BaekjoonHub

--- 백준/Gold/16932. 모양 만들기/모양 만들기.java
+++ 백준/Gold/16932. 모양 만들기/모양 만들기.java
@@ -1,16 +1,14 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
-    private static boolean[][] visit;
-    private static int[][] board, temp;
-    private static List<Integer> count;
-    private static final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
-    private static int n, m;
+    static int n, m;
+    static int[] dy = { 0, 0, 1, -1 }, dx = { 1, -1, 0, 0 }, numbers, count;
+    static int[][] board;
+    static boolean[][] visit;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
@@ -19,87 +17,86 @@ class Main {
 
         board = new int[n][m];
         List<int[]> zeros = new ArrayList<>();
-        for(int i=0 ; i<n ; i++){
+        List<int[]> ones = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             tokenizer = new StringTokenizer(br.readLine());
 
-            for(int j=0 ; j<m ; j++){
+            for (int j = 0; j < m; j++) {
                 board[i][j] = Integer.parseInt(tokenizer.nextToken());
 
-                if(board[i][j] == 0)
-                    zeros.add(new int[]{i, j});
+                if (board[i][j] == 1)
+                    ones.add(new int[] { i, j });
+                else
+                    zeros.add(new int[] { i, j });
             }
         }
-        
+
+        int number = 2;
+        count = new int[n * m + 3];
         visit = new boolean[n][m];
-        count = new ArrayList<>();
-        count.add(0);
+        for (int[] pos : ones) {
+            int y = pos[0], x = pos[1];
 
-        temp = new int[n][m];
-        int number = 1;
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<m ; j++){
-                if(board[i][j] == 0 || visit[i][j])
-                    continue;
+            if (visit[y][x])
+                continue;
 
-                numbering(i, j, number);
-                number++;
-            }
+            bfs(y, x, number++);
         }
 
-        int answer = 0;
-        for(int[] zero : zeros){
-            int y = zero[0], x = zero[1];
+        int maxValue = 0;
+        for(int[] pos : zeros){
+            int y = pos[0], x = pos[1];
+            Map<Integer, Boolean> exist = new HashMap<>();
+            int total = 0;
 
-            int total = 1;
-            Map<Integer, Boolean> already = new HashMap<>();
             for(int i=0 ; i<4 ; i++){
                 int ny = y + dy[i], nx = x + dx[i];
 
-                if(!isIn(ny, nx) || already.containsKey(temp[ny][nx]) || board[ny][nx] == 0)
+                if(!isIn(ny, nx) || exist.containsKey(board[ny][nx]) || board[ny][nx] == 0)
                     continue;
 
-                already.put(temp[ny][nx], true);
-                total += count.get(temp[ny][nx]);
+                exist.put(board[ny][nx], true);
+                total += count[board[ny][nx]];
             }
 
-            answer = Math.max(answer, total);
+            maxValue = Math.max(maxValue, total);
         }
 
-        bw.write(String.valueOf(answer));
-        bw.close();
+        System.out.println(maxValue + 1);
+
         br.close();
     }
 
-    private static void numbering(int startY, int startX, int number){
-        Queue<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{startY, startX});
+    static boolean isIn(int y, int x){
+        return 0 <= y && y < n && 0 <= x && x < m;
+    }
 
+    static void bfs(int startY, int startX, int number) {
         visit[startY][startX] = true;
 
-        int cnt = 0;
+        Queue<int[]> que = new ArrayDeque<>();
+        que.add(new int[] { startY, startX });
 
-        while(!que.isEmpty()) {
+        int ret = 0;
+        while (!que.isEmpty()) {
             int[] cur = que.poll();
 
             int y = cur[0], x = cur[1];
-            temp[y][x] = number;
-            cnt++;
+            board[y][x] = number;
+            ret++;
 
-            for(int i=0 ; i<4 ; i++){
+            for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i], nx = x + dx[i];
 
-                if(!isIn(ny, nx) || visit[ny][nx] || board[ny][nx] == 0)
+                if (!isIn(ny, nx) || visit[ny][nx] || board[ny][nx] == 0)
                     continue;
 
                 visit[ny][nx] = true;
-                que.add(new int[]{ny, nx});
+                que.add(new int[] { ny, nx });
             }
         }
 
-        count.add(cnt);
-    }
-
-    private static boolean isIn(int y, int x){
-        return 0 <= y && y < n && 0 <= x && x < m;
+        count[number] = ret;
     }
 }
