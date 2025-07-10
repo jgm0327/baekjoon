72b80a814e072cf7f7677330d04f2ab3010cf6a4
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-07-10 09:40:02 +0900

    [Platinum V] Title: 카드 게임, Time: 1380 ms, Memory: 380760 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.java
+++ 백준/Platinum/16566. 카드 게임/카드 게임.java
@@ -1,74 +1,82 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
-    static int[] cards;
-    static int[] parents;
-    public static void main(String args[]) throws Exception {
+
+    static int n, m, k;
+    static int[] parents, cards;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(tokenizer.nextToken());
-        int m = Integer.parseInt(tokenizer.nextToken());
-        int k = Integer.parseInt(tokenizer.nextToken());
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+        k = Integer.parseInt(tokenizer.nextToken());
 
-        cards = new int[m];
         tokenizer = new StringTokenizer(br.readLine());
-        for(int i=0 ; i<m ; i++)
+        cards = new int[m];
+        for (int i = 0; i < m; i++)
             cards[i] = Integer.parseInt(tokenizer.nextToken());
-
         Arrays.sort(cards);
 
-        parents = new int[m];
-        for(int i=0 ; i<m ; i++)
+        parents = new int[m + 1];
+        for (int i = 0; i <= m; i++)
             parents[i] = i;
 
         tokenizer = new StringTokenizer(br.readLine());
         StringBuilder answer = new StringBuilder();
-        while(tokenizer.hasMoreTokens()){
-            int num = Integer.parseInt(tokenizer.nextToken());
+        while (k-- > 0) {
+            int number = Integer.parseInt(tokenizer.nextToken());
 
-            int idx = bisectLeft(num);
-            int pIdx = findParent(idx);
+            int idx = bisectLeft(number);
 
-            answer.append(cards[pIdx]).append("\n");
-            if(pIdx + 1 < m) union(idx, pIdx + 1);
+            // 2 3 4 5 7 8 9
+            int nextCard = findParent(idx);
+            if(cards[nextCard] == number)
+                nextCard = findParent(nextCard + 1);
+
+            if(nextCard >= m)
+                continue;
+
+            answer.append(cards[nextCard]).append("\n");
+            union(nextCard, findParent(nextCard + 1));
         }
 
         System.out.print(answer);
-
         br.close();
     }
 
-    static int findParent(int x){
-        if(x == parents[x])
-            return x;
-
-        return parents[x] = findParent(parents[x]);
-    }
-
-    static void union(int x, int y){
-        int px = findParent(x), py = findParent(y);
-
-        if(px == py)
-            return;
-
-        parents[px] = py;
-    }
-
-    static int bisectLeft(int target){
-        int left = 0, right = cards.length - 1;
-
-        while(left <= right){
+    static int bisectLeft(int target) {
+        int left = 0, right = m - 1;
+        while (left <= right) {
             int mid = (left + right) / 2;
 
-            if(cards[mid] <= target)
-                left = mid + 1;
-            else
+            if (target <= cards[mid])
                 right = mid - 1;
+            else
+                left = mid + 1;
         }
 
         return left;
+    }
+
+    static int findParent(int x) {
+        if (x == parents[x])
+            return x;
+        return parents[x] = findParent(parents[x]);
+    }
+
+    static void union(int x, int y) {
+        int px = findParent(x), py = findParent(y);
+
+        if (px == py)
+            return;
+
+        parents[px] = py;
     }
 }
