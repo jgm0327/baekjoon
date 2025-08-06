f641d4e21df0047204551fc4f4fab1e88cc4fcc1
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-08-06 09:06:23 +0900

    [Gold IV] Title: 알파벳, Time: 716 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,50 +2,46 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-    private static int n, m, answer;
-    private static char[][] board;
-    private static final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
+class Main {
+    static int n, m, answer;
+    static int[] dy = { 0, 0, 1, -1 }, dx = { 1, -1, 0, 0 };
+    static char[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] size = br.readLine().split(" ");
-        n = Integer.parseInt(size[0]);
-        m = Integer.parseInt(size[1]);
+        String[] str = br.readLine().split(" ");
+
+        n = Integer.parseInt(str[0]);
+        m = Integer.parseInt(str[1]);
 
         board = new char[n][m];
-        for(int i=0 ; i<n ; i++){
-            String str = br.readLine();
-            for(int j=0 ; j<m ; j++){
-                board[i][j] = str.charAt(j);
-            }
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++)
+                board[i][j] = s.charAt(j);
         }
 
-        answer = 0;
-        dfs(1, 0, 0, 1 << (int)(board[0][0] - 'A'));
-
+        dfs(0, 0, (1 << (board[0][0] - 'A')), 1);
         System.out.println(answer);
+
+        br.close();
     }
 
-    public static void dfs(int depth, int y, int x, int path){
+    static void dfs(int y, int x, int visit, int depth) {
+        answer = Math.max(answer, depth);
 
-        answer = Math.max(depth, answer);
-        
         for(int i=0 ; i<4 ; i++){
             int ny = y + dy[i], nx = x + dx[i];
-            
-            if(0 > ny || ny >= n || 0 > nx || nx >= m)
-                continue;
-            
-            int bit = (int)(board[ny][nx] - 'A');
 
-            if((path & (1 << bit)) == (1 << bit))
+            if(0 > ny || ny >= n || 0 > nx || nx >= m )
                 continue;
 
-            path |= (1 << bit);
-            dfs(depth + 1, ny, nx, path);
-            path &= ~(1 << bit);
+            int idx = board[ny][nx] - 'A';
+            if((visit & (1 << idx)) != 0)
+                continue;
+
+            dfs(ny, nx, visit | (1 << idx), depth + 1);
         }
     }
 }
