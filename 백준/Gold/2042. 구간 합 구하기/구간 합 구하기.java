964ce0f749aecb8b1d73046409780f82c44f1f67
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-09-03 16:14:59 +0900

    [Gold I] Title: 구간 합 구하기, Time: 472 ms, Memory: 116912 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
-
     static int n, m, k;
-    static long[] tree, arr;
+    static long[] arr, tree;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,73 +15,74 @@ class Main {
         m = Integer.parseInt(tokenizer.nextToken());
         k = Integer.parseInt(tokenizer.nextToken());
 
-        int h = (int) Math.ceil(Math.log(n) / Math.log(2));
-        int size = (1 << (h + 1));
-
-        tree = new long[size];
         arr = new long[n + 1];
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
             arr[i] = Long.parseLong(br.readLine());
-        }
+
+        int h = (int) Math.ceil(Math.log(n) / Math.log(2)) + 1;
+        tree = new long[(1 << h)];
 
         makeSegmentTree(1, n, 1);
 
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < m + k; i++) {
+        StringBuilder ansewr = new StringBuilder();
+        while (m-- > 0 || k-- > 0) {
             tokenizer = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(tokenizer.nextToken());
             int b = Integer.parseInt(tokenizer.nextToken());
             long c = Long.parseLong(tokenizer.nextToken());
 
-            if (a == 1)
-                updateTree(1, n, 1, b, c);
-
+            if (a == 1) {
+                updateSegmentTree(1, n, 1, b, c);
+            }
             else
-                answer.append(getSum(1, n, 1, b, (int) c)).append("\n");
-
+                ansewr.append(getSum(1, n, 1, b, (int)c)).append("\n");
         }
-        System.out.print(answer);
+
+        System.out.print(ansewr);
 
         br.close();
     }
 
-    static void makeSegmentTree(int left, int right, int depth) {
+    static void makeSegmentTree(int left, int right, int idx) {
         if (left == right) {
-            tree[depth] = arr[left];
+            tree[idx] = arr[left];
             return;
         }
 
         int mid = (left + right) / 2;
 
-        makeSegmentTree(left, mid, depth * 2);
-        makeSegmentTree(mid + 1, right, depth * 2 + 1);
+        makeSegmentTree(left, mid, idx * 2);
+        makeSegmentTree(mid + 1, right, idx * 2 + 1);
 
-        tree[depth] = tree[2 * depth] + tree[2 * depth + 1];
+        tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
     }
 
-    static void updateTree(int left, int right, int depth, int target, long value) {
-        if(left == right){
-            tree[depth] = value;
+    static void updateSegmentTree(int left, int right, int idx, int target, long value) {
+        if (left == right && target == left) {
+            tree[idx] = value;
             arr[target] = value;
             return;
         }
 
         int mid = (left + right) / 2;
 
-        if(target <= mid)updateTree(left, mid, depth * 2, target, value);
-        else updateTree(mid + 1, right, depth * 2 + 1, target, value);
+        if(target <= mid) updateSegmentTree(left, mid, idx * 2, target, value);
+        else updateSegmentTree(mid + 1, right, idx * 2 + 1, target, value);
 
-        tree[depth] = tree[2 * depth] + tree[2 * depth + 1];
+        tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
     }
 
-    static long getSum(int left, int right, int depth, int start, int end) {
-        if(left > end || right < start) return 0;
-        if(start <= left && right <= end) return tree[depth];
+    static long getSum(int left, int right, int idx, int start, int end){
+        if(left > end || right < start){
+            return 0;
+        }
+
+        if(start <= left && right <= end)
+            return tree[idx];
 
         int mid = (left + right) / 2;
 
-        return getSum(left, mid, depth * 2, start, end) + getSum(mid + 1, right, depth * 2 + 1, start, end);
+        return getSum(left, mid, idx * 2, start, end) + getSum(mid + 1, right, idx * 2 + 1, start, end);
     }
 }
