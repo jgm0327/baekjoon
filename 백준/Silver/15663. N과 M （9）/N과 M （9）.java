da8600bdeb13fc483e8c0f3e7d1bc15e76644883
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-09-12 15:21:22 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 19244 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,55 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-	private static int n, m;
-	private static int[] arr;
-	private static StringBuilder answer;
+class Main {
+    static int[] arr;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(tokenizer.nextToken());
-		m = Integer.parseInt(tokenizer.nextToken());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-		tokenizer = new StringTokenizer(br.readLine());
-		arr = new int[n];
-		for(int i=0 ; i<n ; i++){
-			arr[i] = Integer.parseInt(tokenizer.nextToken());
-		}
+        int n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
 
-		Arrays.sort(arr);
+        tokenizer = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for(int i=0 ; i<n ; i++){
+            arr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
 
-		answer = new StringBuilder();
-		dfs(0, new int[m], new boolean[n]);
+        Arrays.sort(arr);
 
-		System.out.print(answer);
-		br.close();
-	}
+        StringBuilder answer = new StringBuilder();
+        dfs(0, m, new int[m], answer, new boolean[n]);
 
-	private static void dfs(int depth, int[] path, boolean[] visit){
-		if(depth == m){
-			for(int i=0 ; i<m ; i++){
-				answer.append(path[i]).append(" ");
-			}
-			answer.append("\n");
-			return;
-		}
+        System.out.print(answer);
 
-		for(int i=0 ; i<n ; i++){
-			if(visit[i] || path[depth] == arr[i])
-				continue;
+        br.close();
+    }
 
-			visit[i] = true;
-			path[depth] = arr[i];
-			dfs(depth + 1, path, visit);
-			visit[i] = false;
-		}
-		path[depth] = 0;
-	}
+    static void dfs(int idx, int m, int[] path, StringBuilder answer, boolean[] visit){
+        if(m == 0){
+            for(int num : path){
+                answer.append(num).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for(int i=0 ; i<arr.length ; i++){
+            if(visit[i] || path[idx] == arr[i])
+                continue;
+
+            path[idx] = arr[i];
+            visit[i] = true;
+            dfs(idx + 1, m - 1, path, answer, visit);
+            visit[i] = false;
+        }
+        
+        path[idx] = 0;
+    }
 }
