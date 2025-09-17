d565a8f1c378955bb151414bd3533e40d69c5d61
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-09-17 11:10:07 +0900

    [Gold III] Title: 내리막 길, Time: 284 ms, Memory: 39372 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,53 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main {
-	private static int n, m, answer;
-	private static int[][] graph, dp;
-	private static int[] dy, dx;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] size = br.readLine().split(" ");
-		n = Integer.parseInt(size[0]);
-		m = Integer.parseInt(size[1]);
-		
-		graph = new int[n][m];
-		for(int i=0 ; i<n ; i++) {
-			String[] values = br.readLine().split(" ");
-			for(int j=0 ; j<m ; j++) {
-				graph[i][j] = Integer.parseInt(values[j]);
-			}
-		}
-		
-		dp = new int[n][m];
-		for(int i=0 ; i<n ; i++) {
-			Arrays.fill(dp[i], -1);
-		}
-		
-		dy = new int[] {0,0,1,-1};
-		dx = new int[] {1,-1,0,0};
-		System.out.println(dfs(0, 0));
-		
-		br.close();
-	}
-	
-	private static int dfs(int y, int x) {
-		if(y == n - 1 && x == m - 1) {
-			return 1;
-		}
-		if(dp[y][x] != -1)
-			return dp[y][x];
-		
-		dp[y][x] = 0;
-		for(int i=0 ; i<4 ; i++) {
-			int ny = y + dy[i], nx = x + dx[i];
-			if((0 <= ny && ny < n && 0 <= nx && nx < m) && graph[y][x] > graph[ny][nx]) {
-				dp[y][x] += dfs(ny, nx);
-			}
-		}
-		return dp[y][x];
-	}
+class Main {
+    static int n, m;
+    static int[] dy = { 0, 0, 1, -1 }, dx = { 1, -1, 0, 0 };
+    static int[][] board, dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+
+        board = new int[n][m];
+        dp = new int[n][m];
+        for(int i=0 ; i<n ; i++){
+            tokenizer = new StringTokenizer(br.readLine());
+
+            for(int j=0 ; j<m ; j++){
+                board[i][j] = Integer.parseInt(tokenizer.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
+
+        br.close();
+    }
+
+    static int dfs(int y, int x){
+        if(y == n - 1 && x == m - 1){
+            return 1;
+        }
+
+        if(dp[y][x] != -1)
+            return dp[y][x];
+
+        dp[y][x] = 0;
+        for(int i=0 ; i<4 ; i++){
+            int ny = y + dy[i], nx = x + dx[i];
+            if(0 > ny || ny >= n || 0 > nx || nx >= m || board[y][x] <= board[ny][nx])
+                continue;
+
+            dp[y][x] += dfs(ny, nx);
+        }
+
+
+        return dp[y][x];
+    }
 }
