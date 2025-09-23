5f731a0c6ece8873e0792bb60dcf063b3409e3d6
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2025-09-24 08:25:22 +0900

    [level 3] Title: 입국심사, Time: 52.80 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 75.10 ms
+메모리: 91.6 MB, 시간: 52.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 19일 17:16:07
+2025년 09월 24일 08:25:18
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,8 +1,6 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        
-        long left = 1, right = (long)1e18 + 1;
+        long left = 1, right = (long)1e18;
         
         while(left <= right){
             long mid = (left + right) / 2;
@@ -12,14 +10,12 @@ class Solution {
                 total += mid / time;
             }
             
-            // System.out.println(total + " " + mid);
-            
-            if(total >= n){
+            if(total >= n)
                 right = mid - 1;
-            }
             else
                 left = mid + 1;
         }
+        
         return left;
     }
 }
