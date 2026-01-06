e07809fd0dd7ec5f3373b537777b89df776065d6
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-01-06 23:33:35 +0900

    [level 2] Title: 단체사진 찍기, Time: 342.08 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,78 +1,71 @@
 import java.util.*;
 
 class Solution {
-    Map<String, Integer> chIdx;
-    final String[] names = {"A", "C", "F", "J", "M", "N", "R", "T"};
-    int answer = 0;
-    
-    class Dist{
-        String name1, name2;
-        char comp;
+    class Distance{
+        int name1, name2;
         int dist;
+        char sign;
         
-        public Dist(String name1, String name2, char comp, int dist){
+        public Distance(int name1, int name2, char sign, int dist){
             this.name1 = name1;
             this.name2 = name2;
-            this.comp = comp;
+            this.sign = sign;
             this.dist = dist;
         }
     }
     
+    Distance[] distances;
+    int[] idx;
+    char[] names = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    int answer = 0;
     public int solution(int n, String[] data) {
-        chIdx = new HashMap<>();
+        distances = new Distance[n];
+        idx = new int[26];
         
-        for(int i=0 ; i<8 ; i++){
-            chIdx.put(names[i], -1);
+        for(int i=0 ; i<data.length ; i++){
+            int first = data[i].charAt(0) - 'A';
+            int second = data[i].charAt(2) - 'A';
+            char sign = data[i].charAt(3);
+            int dist = data[i].charAt(4) - '0' + 1;
+            
+            distances[i] = new Distance(first, second, sign, dist);
         }
         
-        Dist[] dists = new Dist[n];
-        for(int i=0 ; i<n ; i++){
-            char ch = data[i].charAt(3);
-            
-            String[] splitNumber = data[i].split(Character.toString(ch));
-            int dist = Integer.parseInt(splitNumber[1]);
-            
-            String[] splitName = splitNumber[0].split("~");
-            
-            dists[i] = new Dist(splitName[0], splitName[1], ch, dist);
-        }
-        
-        dfs(0, dists);
-        return answer;
+        return dfs(0, n, 0);
     }
     
-    private void dfs(int depth, final Dist[] dists){
+    int dfs(int visit, int n, int depth){
         if(depth == 8){
-            if(canGetLine(dists)){
-                answer++;
-            }
-            return;
+            if(canMakeLine(n)) return 1;
+            return 0;
         }
         
-        for(String name : names){
-            if(chIdx.get(name) != -1)continue;
+        int ret = 0;
+        for(int i=0 ; i<8 ; i++){
+            if((visit & (1 << i)) != 0)
+                continue;
             
-            chIdx.put(name, depth);
-            dfs(depth + 1, dists);
-            chIdx.put(name, -1);
+            idx[names[i] - 'A'] = depth;
+            ret += dfs(visit | (1 << i), n, depth + 1);
         }
+        
+        return ret;
     }
     
-    private boolean canGetLine(final Dist[] dists){
-        for(Dist dist : dists){
-            String name1 = dist.name1, name2 = dist.name2;
-            char comp = dist.comp;
-            int d = Math.abs(chIdx.get(name1) - chIdx.get(name2)) - 1;
+    boolean canMakeLine(int n){
+        for(int i=0 ; i<n ; i++){
+            int f = distances[i].name1;
+            int s = distances[i].name2;
+            int dist = distances[i].dist;
+            char sign = distances[i].sign;
             
-            if(comp == '>' && d <= dist.dist){
+            if(sign == '>' && Math.abs(idx[f] - idx[s]) <= dist)
                 return false;
-            }else if(comp == '<' && d >= dist.dist){
+            else if(sign == '<' && Math.abs(idx[f] - idx[s]) >= dist)
                 return false;
-            }else if(comp == '=' && dist.dist != d){
-                return false;
-            }
+            else if(sign == '=' && Math.abs(idx[f] - idx[s]) != dist)
+                return false;  
         }
         return true;
     }
 }
-    
