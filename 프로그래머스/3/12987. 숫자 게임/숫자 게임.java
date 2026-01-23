35cbb1b1fc5bd77bfef777635e7ba4f020186ae3
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-01-23 14:49:16 +0900

    [level 3] Title: 숫자 게임, Time: 162.67 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,32 +1,41 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
-        
-        int left = 0, right = A.length - 1;
-        
-        for(int number : A){
-            left = bisectLeft(B, left, number);
+        /*
+            1 3 5 7
+            2 2 6 8
+        */
+        boolean[] visit = new boolean[A.length];
+        for(int i=0 ; i<B.length ; i++){
+            int idx = bisectLeft(B[i], A);
             
-            if(left < B.length && B[left] > number){
-                answer++;
-                left++;
+            if(idx < 0)
+                continue;
+            
+            idx = Math.min(idx, A.length - 1);
+            while(idx >= 0 && (visit[idx] || A[idx] >= B[i])){
+                idx--;
             }
+            
+            if(idx < 0)
+                continue;
+            
+            visit[idx] = true;
+            answer++;
         }
-        
         return answer;
     }
     
-    private int bisectLeft(int[] B, int start, int target){
-        int left = start, right = B.length - 1;
-        
+    int bisectLeft(int target, int[] arr){
+        int left = 0, right = arr.length - 1;
         while(left <= right){
             int mid = (left + right) / 2;
             
-            if(B[mid] > target)
+            if(target < arr[mid])
                 right = mid - 1;
             else
                 left = mid + 1;
