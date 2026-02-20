a7e3f75dc9ae236a3bfa7d7464868943b83d1bc1
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-02-20 23:46:10 +0900

    [level 3] Title: 입국심사, Time: 63.32 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,21 +1,18 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long left = 1, right = (long)1e18;
-        
+        long left = 0, right = (long)1e18;
         while(left <= right){
             long mid = (left + right) / 2;
             
             long total = 0;
             for(int time : times){
-                total += mid / time;
+                total += (mid / time);
             }
             
             if(total >= n)
                 right = mid - 1;
-            else
-                left = mid + 1;
+            else left = mid + 1;
         }
-        
         return left;
     }
 }
