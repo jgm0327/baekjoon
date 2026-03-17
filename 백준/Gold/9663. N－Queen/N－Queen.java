f6e65c291d82e586a4b25e73e53ab428acb43337
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-03-17 20:04:22 +0900

    [Gold IV] Title: N-Queen, Time: 10084 ms, Memory: 12716 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,42 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-class Main{
-
-    private static int n, answer;
-    private static int[] board;
-
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-
-        board = new int[n];
-
-        answer = 0;
-        backtracking(0);
-        System.out.println(answer);
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(dfs(n, 0, new int[n], 0));
+        br.close();
     }
 
-    private static void backtracking(int depth){
-        if(depth == n){
-            answer++;
-            return;
+    static int dfs(int n, int y, int[] visit, int visitX) {
+        if (y == n)
+            return 1;
+
+        int ret = 0;
+        for (int x = 0; x < n; x++) {
+            int curBit = (1 << x);
+
+            if (!check(y, x, visit) || (visitX & curBit) != 0)
+                continue;
+
+            visit[y] = visit[y] | curBit;
+            ret += dfs(n, y + 1, visit, visitX | curBit);
+            visit[y] = visit[y] & (~curBit);
         }
 
-        for(int i=0 ; i<n ; i++){
-            board[depth] = i;
-            if(!check(depth))continue;
-            backtracking(depth + 1);
-        }
-        
+        return ret;
     }
-    
-    private static boolean check(int y){
-        for(int i=0 ; i<y ; i++){
-            if(board[y] == board[i])return false;
-            if(y - i == Math.abs(board[y] - board[i]))return false;
+
+    static boolean check(int y, int x, int[] visit) {
+        for (int i = 1; i < visit.length; i++) {
+            int ny = y - i;
+            int leftX = x - i;
+            int rightX = x + i;
+
+            if (ny < 0 || (leftX < 0 && rightX >= visit.length))
+                return true;
+
+            if ((visit[ny] & (1 << leftX)) != 0 || (visit[ny] & (1 << rightX)) != 0)
+                return false;
         }
 
         return true;
