80c7ba98d0cdb911ab2a89d88ca4a31df1bc6daa
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-03-19 19:39:40 +0900

    [Gold V] Title: 인내의 도미노 장인 호석, Time: 60 ms, Memory: 20152 KB, Score: 2 point -BaekjoonHub

--- 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
+++ 백준/Gold/20165. 인내의 도미노 장인 호석/인내의 도미노 장인 호석.java
@@ -2,98 +2,87 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int n, m;
+    static int[] dy = { 0, 0, 1, -1 }, dx = { 1, -1, 0, 0 };
+    static int[][] board;
+    static boolean[][] isFallen;
 
-    private static int n, m;
-    private static int[][] board;
-    private static char[][] state;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
         n = Integer.parseInt(tokenizer.nextToken());
         m = Integer.parseInt(tokenizer.nextToken());
-        int r = Integer.parseInt(tokenizer.nextToken());
+        int R = Integer.parseInt(tokenizer.nextToken());
 
         board = new int[n][m];
-        state = new char[n][m];
-
-        for(int i=0 ; i<n ; i++){
+        for (int i = 0; i < n; i++) {
             tokenizer = new StringTokenizer(br.readLine());
-
-            for(int j=0 ; j<m ; j++){
+            for (int j = 0; j < m; j++) {
                 board[i][j] = Integer.parseInt(tokenizer.nextToken());
-                state[i][j] = 'S';
             }
         }
 
-        Map<Character, Integer> index = Map.of('E', 0, 'W', 1, 'S', 2, 'N', 3);
-        int answer = 0;
-
-        for(int i=0 ; i<r * 2 ; i++){
+        isFallen = new boolean[n][m];
+        int total = 0;
+        while (R-- > 0) {
             tokenizer = new StringTokenizer(br.readLine());
 
-            int y = Integer.parseInt(tokenizer.nextToken());
-            int x = Integer.parseInt(tokenizer.nextToken());
+            int r = Integer.parseInt(tokenizer.nextToken()) - 1;
+            int c = Integer.parseInt(tokenizer.nextToken()) - 1;
+            int dir = getDir(tokenizer.nextToken().charAt(0));
 
-            if(i % 2 == 0){
-                char dir = tokenizer.nextToken().charAt(0);
-                answer += attackTurn(y - 1, x - 1, index.get(dir));
-                continue;
-            }
+            total += offense(r, c, dir);
 
-            state[y - 1][x - 1] = 'S';
+            tokenizer = new StringTokenizer(br.readLine());
+
+            r = Integer.parseInt(tokenizer.nextToken()) - 1;
+            c = Integer.parseInt(tokenizer.nextToken()) - 1;
+
+            isFallen[r][c] = false;
         }
-        bw.append(answer+"\n");
+
+        StringBuilder answer = new StringBuilder();
+        answer.append(total).append("\n");
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                bw.append(state[i][j]).append(' ');
+                if(isFallen[i][j]){
+                    answer.append("F ");
+                }
+                else answer.append("S ");
             }
-            bw.append('\n');
+            answer.append('\n');
         }
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
+
+        br.close();
     }
 
-    private static int attackTurn(int y, int x, int dir){   
-        final int[] dy = {0,0,1,-1}, dx = {1,-1,0,0};
-        int count = 0;
+    static int offense(int r, int c, int dir){
+        if(isFallen[r][c])
+            return 0;
 
-        int endY = y + board[y][x] * dy[dir], endX = x + board[y][x] * dx[dir];
-        int mul = 0;
-
-        while(true){
-            int ny = y + mul * dy[dir], nx = x + mul * dx[dir];
-
-            if(!isIn(ny, nx) || (ny == endY && nx == endX)) break;
-
-            if(state[ny][nx] == 'S'){
-                state[ny][nx] = 'F';
-                count++;
-
-                if(dy[dir] < 0 || dx[dir] < 0){
-                    if(endY > board[ny][nx] * dy[dir] + ny 
-                    || endX > board[ny][nx] * dx[dir] + nx){
-                        endY = board[ny][nx] * dy[dir] + ny;
-                        endX = board[ny][nx] * dx[dir] + nx;
-                    }
-                }
-
-                else if(endY < board[ny][nx] * dy[dir] + ny 
-                || endX < board[ny][nx] * dx[dir] + nx){
-                    endY = board[ny][nx] * dy[dir] + ny;
-                    endX = board[ny][nx] * dx[dir] + nx;
-                }
+        int ret = 0;
+        int size = board[r][c] - 1;
+        while(0 <= c && c < m && 0 <= r && r < n && size > 0){
+            size--;
+            if(!isFallen[r][c]){
+                isFallen[r][c] = true;
+                ret++;
+                size = Math.max(size, board[r][c] - 1);
             }
 
-            mul++;
+            r = r + dy[dir];
+            c = c + dx[dir];
         }
-
-        return count;
+        return ret;
     }
 
-    private static boolean isIn(int y, int x){
-        return 0 <= y && y < n && 0 <= x && x < m;
+    static int getDir(char dir){
+        if(dir == 'E') return 0;
+        if(dir == 'W')return 1;
+        if(dir == 'S')return 2;
+        return 3;
     }
+
 }
