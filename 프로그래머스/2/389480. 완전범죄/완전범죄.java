e3ea2c86ca75918e9b604738f5918130da482e6b
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-04-06 11:22:33 +0900

    [level 2] Title: 완전범죄, Time: 0.84 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,25 +1,31 @@
 class Solution {
-    int answer;
-    boolean[][][] visit;
+    int maxValue = -1;
     public int solution(int[][] info, int n, int m) {
-        answer = Integer.MAX_VALUE;
-        visit = new boolean[info.length + 1][n + 1][m + 1];
-        dfs(info, 0, 0, 0, n, m);
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        int totalB = 0;
+        for(int[] amount : info){
+            totalB += amount[1];
+        }
+        
+        if(totalB < m)
+            return 0;
+        
+        dfs(info, n, m, 0, new boolean[info.length][n + 1][m + 1]);
+        return maxValue == -1 ? -1 : n - maxValue;
     }
     
-    void dfs(int[][] info, int idx, int A, int B, int n, int m){
-        if(A >= n || B >= m || visit[idx][A][B])
+    private void dfs(int[][] info, int A, int B, int idx, boolean[][][] visit){
+        if(A <= 0 || B <= 0 || maxValue >= A || (idx < info.length && visit[idx][A][B]))
             return;
         
-        visit[idx][A][B] = true;
-        
         if(idx == info.length){
-            answer = Math.min(answer, A);
+            maxValue = Math.max(maxValue, A);
             return;
         }
         
-        dfs(info, idx + 1, A + info[idx][0], B, n, m);
-        dfs(info, idx + 1, A, B + info[idx][1], n, m);
+        visit[idx][A][B] = true;
+        
+        dfs(info, A - info[idx][0], B, idx + 1, visit);
+        dfs(info, A, B - info[idx][1], idx + 1, visit);
     }
+        
 }
