d21d5fbf085b1017ea7120fcb2f6e4fb82b97031
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-04-09 11:05:56 +0900

    [Gold V] Title: 치킨 배달, Time: 144 ms, Memory: 16776 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,68 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-	private static int n, m, answer;
-	private static List<int[]> chickenHouses, houses;
-	private static Deque<int[]> open;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().split(" ");
-		n = Integer.parseInt(str[0]);
-		m = Integer.parseInt(str[1]);
-		answer = Integer.MAX_VALUE;
+class Main {
+    static int n;
+    static int[][] board;
+    static List<int[]> houses;
+    static List<int[]> chickenHouses;
 
-		chickenHouses = new ArrayList<>();
-		houses = new ArrayList<>();
-		open = new ArrayDeque<>();
-		
-		for(int i=0 ; i<n ; i++) {
-			String[] input = br.readLine().split(" ");
-			for(int j=0 ; j<n ;j++) {
-				int data = Integer.parseInt(input[j]);
-				if(data == 2)chickenHouses.add(new int[] {i, j});
-				else if(data == 1)houses.add(new int[] {i, j});
-			}
-		}
-		closed(0, 0);
-		System.out.println(answer);
-		br.close();
-	}
-	
-	private static void calcDist() {
-		int comp = 0, tmp;
-		for(int[] house : houses) {
-			int y = house[0], x = house[1];
-			tmp = Integer.MAX_VALUE;
-			Iterator<int[]> it = open.iterator();
-			while(it.hasNext()) {
-				int[] cur = it.next();
-				int cy = cur[0], cx = cur[1];
-				tmp = Math.min(tmp, (Math.abs(y - cy) + Math.abs(x - cx)));
-			}
-			comp += tmp;
-		}
-		
-		answer = Math.min(comp, answer);
-	}
-	
-	private static void closed(int depth, int start) {
-		if(depth == m) {
-			return;
-		}
-		for(int i=start ; i<chickenHouses.size() ; i++) {
-			open.add(chickenHouses.get(i));
-			calcDist();
-			closed(depth + 1, i + 1);
-			open.pollLast();
-		}
-	}
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        int m = Integer.parseInt(tokenizer.nextToken());
+
+        board = new int[n][n];
+        houses = new ArrayList<>();
+        chickenHouses = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            tokenizer = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(tokenizer.nextToken());
+
+                if (board[i][j] == 1)
+                    houses.add(new int[] { i, j });
+                else if (board[i][j] == 2)
+                    chickenHouses.add(new int[] { i, j });
+            }
+        }
+
+        System.out.println(dfs(0, m, new ArrayDeque<>(), 0));
+
+        br.close();
+    }
+
+    static int calculateDistances(ArrayDeque<int[]> que) {
+        int ret = 0;
+        for (int[] house : houses) {
+            int minValue = Integer.MAX_VALUE;
+            for (int[] chickenHouse : que) {
+                minValue = Math.min(minValue,
+                        Math.abs(house[0] - chickenHouse[0]) + Math.abs(house[1] - chickenHouse[1]));
+            }
+            ret += minValue;
+        }
+
+        return ret;
+    }
+
+    static int dfs(int start, int m, ArrayDeque<int[]> que, int visit) {
+        if (m == 0)
+            return calculateDistances(que);
+
+        int ret = Integer.MAX_VALUE;
+        for (int i = start; i < chickenHouses.size(); i++) {
+            if ((visit & (1 << i)) != 0)
+                continue;
+
+            que.add(chickenHouses.get(i));
+            ret = Math.min(dfs(i + 1, m - 1, que, visit | (1 << i)), ret);
+            que.pollLast();
+        }
+
+        return ret;
+    }
 }
