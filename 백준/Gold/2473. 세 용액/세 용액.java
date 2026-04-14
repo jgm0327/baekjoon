5e3a5a2a4881714ac610a14823a5ade32b1d5a5d
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-04-14 13:25:35 +0900

    [Gold III] Title: 세 용액, Time: 528 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,47 +1,57 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-
     static int n;
-    static int[] arr;
+    static long[] liquid;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
 
-        arr = new int[n];
+        liquid = new long[n];
         StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
-
-        Arrays.sort(arr);
-
-        int[] answer = new int[3];
-        long minValue = Long.MAX_VALUE / 10;
-        for (int left = 0; left < n - 2; left++) {
-            int mid = left + 1, right = n - 1;
-
-            while (mid < right) {
-                long total = (long)arr[left] + (long)arr[mid] + (long)arr[right];
-
-                if (Math.abs(total) < minValue) {
-                    minValue = Math.abs(total);
-                    answer = new int[]{arr[left], arr[mid], arr[right]};
-                }
-
-                if(total < 0)
-                    mid++;
-                else right--;
-            }
+        for (int i = 0; i < n; i++) {
+            liquid[i] = Long.parseLong(tokenizer.nextToken());
         }
 
-        StringBuilder aout = new StringBuilder();
-        for(int i=0 ;i<3 ; i++)
-            aout.append(answer[i]).append(" ");
+        Arrays.sort(liquid);
+        long minValue = Long.MAX_VALUE;
+        long[] answer = new long[3];
+        for (int left = 0; left < n - 2; left++) {
+            for (int mid = left + 1; mid < n - 1; mid++) {
+                long total = liquid[left] + liquid[mid];
 
-        System.out.println(aout);
+                int index = bisectLeft(-total, mid + 1);
+                int right = index;
+
+                if (index - 1 > mid && Math.abs(total + liquid[index - 1]) < Math.abs(total + liquid[index]))
+                    right = index - 1;
+                if (index + 1 < n && Math.abs(total + liquid[index + 1]) < Math.abs(total + liquid[right]))
+                    right = index + 1;
+
+                if (minValue > Math.abs(total + liquid[right])) {
+                    minValue = Math.abs(total + liquid[right]);
+                    answer = new long[] { liquid[left], liquid[mid], liquid[right] };
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+            System.out.print(answer[i] + " ");
+
         br.close();
+    }
+
+    static int bisectLeft(long target, int left) {
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (liquid[mid] >= target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return Math.min(left, n - 1);
     }
 }
