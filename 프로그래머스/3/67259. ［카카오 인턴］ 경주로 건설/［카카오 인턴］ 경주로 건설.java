cc50f803150c74dcb0d936263897f18017b08d17
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-07-16 15:54:18 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 399.97 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,67 +1,56 @@
 import java.util.*;
 
 class Solution {
-    class Car{
-        int y, x, dir, cost;
-        
-        public Car(int y, int x, int dir, int cost){
-            this.y = y;
-            this.x = x;
-            this.dir = dir;
-            this.cost = cost;
-        }
-    }
-    
-    final int[] dy = {1, 0, -1, 0}, dx = {0, 1, 0, -1};
     public int solution(int[][] board) {
         int answer = 0;
+        for(int[] b : board)
+            System.out.println(Arrays.toString(b));
         return bfs(board);
     }
     
     int bfs(int[][] board){
-        int n = board.length;
+        int n = board.length, m = board[0].length;
         
-        Queue<Car> que = new ArrayDeque<>();
-        que.add(new Car(0, 0, 0, 0));
-        que.add(new Car(0, 0, 1, 0));
+        int[][][] dp = new int[n][m][4];
+        for(int i=0 ; i<n ; i++)
+            for(int j=0 ; j<m ; j++)
+                Arrays.fill(dp[i][j], Integer.MAX_VALUE);
         
-        int[][][] dp = new int[n][n][4];
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                Arrays.fill(dp[i][j], Integer.MAX_VALUE);       
-            }
-        }
+        Arrays.fill(dp[0][0], 0);
         
-        for(int i=0 ; i<4 ; i++)
-            dp[0][0][i] = 0;
+        int[] dy = {0, 0, -1, 1}, dx = {1, -1, 0, 0};
+        Queue<int[]> que = new ArrayDeque<>();
+        // y, x, cost, dir
+        que.add(new int[]{0, 0, 0, 4});
         
         while(!que.isEmpty()){
-            Car car = que.poll();
+            int[] cur = que.poll();
             
+            int y = cur[0], x = cur[1], cost = cur[2], dir = cur[3];
             for(int i=0 ; i<4 ; i++){
-                int ny = car.y + dy[i], nx = car.x + dx[i];
-                int cost = 100;
+                int ny = y + dy[i], nx = x + dx[i];
                 
-                if(i != car.dir) 
-                    cost = 600;
-                
-                if(0 > ny || ny >= n || 0 > nx || nx >= n
-                   || board[ny][nx] == 1 || dp[ny][nx][i] <= car.cost + cost)
+                if(0 > ny || ny >= n || 0 > nx || nx >= m || board[ny][nx] == 1)
                     continue;
                 
-                dp[ny][nx][i] = car.cost + cost;
-                if(ny == n - 1 && nx == n - 1)
+                int nextCost = cost + 100;
+                if(dir != i && dir != 4)
+                    nextCost += 500;
+                
+                if(nextCost > dp[ny][nx][i])
                     continue;
                 
-                que.add(new Car(ny, nx, i, dp[ny][nx][i]));
+                dp[ny][nx][i] = nextCost;
+                if(ny == n - 1 && nx == m - 1)
+                    continue;
+                que.add(new int[]{ny, nx, nextCost, i});
             }
         }
         
-        int ret = Integer.MAX_VALUE;
+        int ret = dp[n - 1][m - 1][0];
         for(int i=0 ; i<4 ; i++){
-            ret = Math.min(dp[n - 1][n - 1][i], ret);
+            ret = Math.min(ret, dp[n - 1][m - 1][i]);
         }
-        
         return ret;
     }
 }
