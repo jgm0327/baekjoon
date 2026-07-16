25876612b0f1f7363dcacd6337f1b63e9b660439
jang gyu min <100139942+jgm0327@users.noreply.github.com>
2026-07-16 16:01:09 +0900

    [level 3] Title: 순위, Time: 6.94 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -2,52 +2,53 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] results) {
-        int answer = 0;
-        int[][] win = new int[n + 1][n + 1];
-        int[][] lose = new int[n + 1][n + 1];
+        List<Integer>[] graph = new ArrayList[n + 1];
+        List<Integer>[] reverse = new ArrayList[n + 1];
         
-        for(int i=0 ; i<=n ; i++){
-            Arrays.fill(win[i], Integer.MAX_VALUE);
-            Arrays.fill(lose[i], Integer.MAX_VALUE);
+        for(int i=1; i<=n ; i++){
+            graph[i] = new ArrayList<>();
+            reverse[i] = new ArrayList<>();
         }
         
         for(int[] result : results){
-            int sour = result[0];
-            int des = result[1];
+            int a = result[0], b = result[1];
             
-            win[sour][des] = 1;
-            lose[des][sour] = 1;
+            graph[a].add(b);
+            reverse[b].add(a);
         }
         
-        for(int k = 1 ; k <= n ; k++){
-            for(int i = 1 ; i <= n ; i++){
-                for(int j = 1 ; j <= n ; j++){
-                    if(win[i][k] != Integer.MAX_VALUE 
-                       && win[k][j] != Integer.MAX_VALUE){
-                        
-                        win[i][j] = 1;
-                    }
-                    
-                    if(lose[i][k] != Integer.MAX_VALUE
-                      && lose[k][j] != Integer.MAX_VALUE){
-                        lose[i][j] = 1;
-                    }
-                }
-            }
-        }
-        
+        int answer = 0;
         for(int i=1 ; i<=n ; i++){
-            int total = 0;
-            for(int j=1 ; j<=n ; j++){
-                if(win[i][j] == Integer.MAX_VALUE 
-                   && lose[i][j] == Integer.MAX_VALUE)continue;
-                total++;
-            }
-            
-            if(total != n - 1)continue;
-            answer++;
+            if(bfs(i, graph) + bfs(i, reverse) == n - 1)
+                answer++;
         }
         
         return answer;
     }
+    
+    int bfs(int start, List<Integer>[] graph){
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(start);
+        
+        boolean[] visit = new boolean[graph.length];
+        visit[start] = true;
+        
+        int ret = 0;
+        while(!que.isEmpty()){
+            int cur = que.poll();
+            
+            for(int next : graph[cur]){
+                if(visit[next])
+                    continue;
+                
+                ret++;
+                visit[next] = true;
+                que.add(next);
+            }
+        }
+        
+        return ret;
+    }
+    
+    
 }
